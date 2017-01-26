2dc612339111f27288a68c3b25f24d07b4e3322c
Alexander Litty <github@alexlitty.com>
2017-01-26 00:50:51 -0800

    Interaction with the program's default log.

--- include/randar/Log/DefaultLog.hpp
+++ include/randar/Log/DefaultLog.hpp
@@ -5,7 +5,26 @@
 
 namespace randar
 {
+    /**
+     * Retrieves the default log for this program.
+     */
     Log& getDefaultLog();
+
+    /**
+     * Creates a generic message on the default log.
+     */
+    void log(const std::string& level, const std::string& contents);
+
+    /**
+     * Creates an informational message on the default log.
+     */
+    void log(const std::string& contents);
+    void logInfo(const std::string& contents);
+
+    /**
+     * Creates an error message on the default log.
+     */
+    void logError(const std::string& contents);
 }
 
 #endif
