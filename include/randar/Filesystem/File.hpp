d05c2e8211f4e18550be2c8612827aa813c99e5a
Alexander Litty <github@alexlitty.com>
2017-02-04 22:32:54 -0800

    Introducing file class.

---
+++ include/randar/Filesystem/File.hpp
@@ -0,0 +1,18 @@
+#ifndef RANDAR_FILESYSTEM_FILE_HPP
+#define RANDAR_FILESYSTEM_FILE_HPP
+
+#include <randar/Filesystem/Path.hpp>
+
+namespace randar
+{
+    class File : public Path
+    {
+    public:
+        /**
+         * Inherit constructors.
+         */
+        using Path::Path;
+    };
+}
+
+#endif
