7f4d352944681deb836b3f5a1938a987dbc1f676
Alexander Litty <github@alexlitty.com>
2017-02-08 02:17:45 -0800

    Added ResourceId structure.

---
+++ include/randar/Project/ResourceId.hpp
@@ -0,0 +1,67 @@
+#ifndef RANDAR_PROJECT_RESOURCE_ID_HPP
+#define RANDAR_PROJECT_RESOURCE_ID_HPP
+
+#include <cstdint>
+
+namespace randar
+{
+    class ResourceId
+    {
+        uint32_t *id;
+
+    public:
+        /**
+         * Default constructor.
+         *
+         * Creates a null resource ID.
+         */
+        ResourceId();
+
+        /**
+         * Constructor.
+         *
+         * Creates a resource ID with the given integer.
+         */
+        ResourceId(uint32_t initid);
+
+        /**
+         * Copy constructor.
+         */
+        ResourceId(const ResourceId& other);
+
+        /**
+         * Move constructor.
+         */
+        ResourceId(ResourceId&& other);
+
+        /**
+         * Destructor.
+         */
+        ~ResourceId();
+
+        /**
+         * Sets the underlying ID.
+         */
+        void set(uint32_t newId);
+
+        /**
+         * Clears the underlying ID, making this a null resource ID.
+         */
+        void clear();
+
+        /**
+         * Checks whether this resource ID is null.
+         */
+        bool isNull() const;
+
+        /**
+         * Retrieves the underlying ID.
+         *
+         * Throws an error if this resource ID is null.
+         */
+        uint32_t get() const;
+        operator uint32_t() const;
+    };
+}
+
+#endif
---
+++ src/Project/ResourceId.cpp
@@ -0,0 +1,71 @@
+#include <stdexcept>
+#include <randar/Project/ResourceId.hpp>
+
+// Default constructor.
+randar::ResourceId::ResourceId()
+: id(nullptr)
+{
+
+}
+
+// Constructor.
+randar::ResourceId::ResourceId(uint32_t initId)
+: id(new uint32_t(initId))
+{
+
+}
+
+// Copy constructor.
+randar::ResourceId::ResourceId(const ResourceId& other)
+: id(other.id == nullptr ? nullptr : new uint32_t(*other.id))
+{
+
+}
+
+// Move constructor.
+randar::ResourceId::ResourceId(ResourceId&& other)
+: id(nullptr)
+{
+    std::swap(this->id, other.id);
+}
+
+// Destructor.
+randar::ResourceId::~ResourceId()
+{
+    this->clear();
+}
+
+// Sets the ID.
+void randar::ResourceId::set(uint32_t newId)
+{
+    this->clear();
+    this->id = new uint32_t(newId);
+}
+
+// Clears the ID, making this a null resource ID.
+void randar::ResourceId::clear()
+{
+    delete this->id;
+    this->id = nullptr;
+}
+
+// Checks whether this resource ID is null.
+bool randar::ResourceId::isNull() const
+{
+    return this->id == nullptr;
+}
+
+// Retrieves the underlying ID.
+uint32_t randar::ResourceId::get() const
+{
+    if (this->isNull()) {
+        throw std::logic_error("Cannot convert null resource ID to integer");
+    }
+
+    return *this->id;
+}
+
+randar::ResourceId::operator uint32_t() const
+{
+    return this->get();
+}
