358fedd26556aeb4966ee2b0e41996a981f8196b
Alexander Litty <github@alexlitty.com>
2017-02-09 00:42:45 -0800

    Modfying go.sh to allow easier testing.

--- go.sh
+++ go.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    make linux64
-else
-    make linux64 -B -j 2
-fi
+
+for var in "$@"
+do
+    if [ "$var" == "--run-tests" ] || [ "$var" == "-t" ]; then
+        run_flags="--test"
+    elif [ "$var" == "--make-all" -o "$var" == "-b" ]; then
+        make_flags="-B -j 2"
+    fi
+done
+
+echo ${run_flags};
+
+make linux64 ${make_flags}
 if [ "$?" -eq "0" ]; then
     cd ./bin/
-    ./randar
+    ./randar ${run_flags}
 fi
