64d6f14923784a622a535b39df97ab982d7b5a02
Alexander Litty <github@alexlitty.com>
2017-02-12 01:14:53 -0800

    Updated debug.js to use new syncing.

--- bin/ui/debug.js
+++ bin/ui/debug.js
@@ -3,24 +3,20 @@ if ((window.location.search || '').indexOf('debug') > -1) {
         console.info("Set monitor target to '" + category + "' #" + name);
     }
 
-    window.getResources = function() {
-        var data = {
-            name: 'Test film',
+    randar.receiveData({
+        name: 'Test film',
 
-            models: {
-                0: "Test model 0",
-                1: "Test model 1",
-                2: "Test model 2"
-            },
+        models: {
+            0: "Test model 0",
+            1: "Test model 1",
+            2: "Test model 2"
+        },
 
-            textures: {
-                0: "Test texture 0",
-                1: "Test texture 1"
-            }
-        };
-
-        return JSON.stringify(data);
-    }
+        textures: {
+            0: "Test texture 0",
+            1: "Test texture 1"
+        }
+    });
 
     window.addEventListener('load', function() {
         randar.ready();
