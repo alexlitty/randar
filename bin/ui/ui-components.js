ce1ae09a22ae2ec3d2113be2baaecdc4ad5c4790
Alexander Litty <github@alexlitty.com>
2017-02-19 05:21:48 -0800

    No more Vue templates/slots in target panel.

--- bin/ui/ui-components.js
+++ bin/ui/ui-components.js
@@ -181,6 +181,7 @@ Component.TargetResourcePanel = combine(
         computed: {
             resource: function() {
                 var result = this.getSelectedResource();
+                result.category = _.clone(this.target.resource.category);
                 result.hasTextures = !!(result.textures && result.textures.length);
                 return result;
             }
@@ -190,12 +191,100 @@ Component.TargetResourcePanel = combine(
             <nav id="target-resource" v-show="isResourceSelected()">
                 <back-button :action="clearTargetResource" />
 
-                <slot
-                    v-bind:name="target.resource.category"
-                    :resource="resource"
-                    :textures="textures"
-                    />
+                <resource-meta :resource="resource" />
+                <editor-model :model="resource" />
             </nav>
         `
     }
 );
+
+/**
+ * A table of resource metadata.
+ */
+Vue.component('resource-meta', {
+    props: {
+        resource: Object
+    },
+
+    computed: {
+        metaProps: function() {
+            if (this.resource.category === 'models') {
+                return {
+                    vertexCount: 'Vertex Count',
+                    faceCount: 'Face Count'
+                };
+            }
+
+            else if (this.resource.category === 'textures') {
+                return {
+                    type: 'Type',
+                    width: 'Width',
+                    height: 'Height'
+                };
+            }
+
+            else {
+                return { };
+            }
+        }
+    },
+
+    template: `
+        <table class="metadata">
+            <thead>
+                <tr>
+                    <th colspan="2">{{ resource.name }}</th>
+                </tr>
+            </thead>
+
+            <tbody v-if="_.size(metaProps)">
+                <tr v-for="(propTitle, propId) in metaProps">
+                    <th>{{ propTitle }}</th>
+                    <td>{{ resource[propId] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    `
+});
+
+/**
+ * An editor for models.
+ */
+Vue.component('editor-model', combine(
+    Component.Common,
+    {
+        props: {
+            model: Object
+        },
+
+        template: `
+            <table class="textures">
+                <thead>
+                    <tr>
+                        <th colspan="2">Textures</th>
+                    </tr>
+                </thead>
+
+                <tbody v-if="model.hasTextures">
+                    <tr class="clickable"
+                     v-for="(textureId, textureSlotId) in model.textures"
+                     @click="target.textureSlotId = textureSlotId">
+                        <th>#{{ textureSlotId }}</th>
+
+                        <td v-if="textures[textureId]">
+                            {{ textures[textureId].name }}
+                        </td>
+
+                        <td v-else class="empty">Unassigned</td>
+                    </tr>
+                </tbody>
+
+                <tbody v-else>
+                    <tr>
+                        <td>No texture slots</td>
+                    </tr>
+                </tbody>
+            </table>
+        `
+    }
+));
