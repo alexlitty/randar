d469eb6f1fd0cfaa412ff01bbd87683651965c8f
Alexander Litty <github@alexlitty.com>
2017-02-20 02:41:21 -0800

    Scenes are now resources.

--- include/randar/Scene/Scene.hpp
+++ include/randar/Scene/Scene.hpp
@@ -1,11 +1,12 @@
 #ifndef RANDAR_SCENE_SCENE_HPP
 #define RANDAR_SCENE_SCENE_HPP
 
+#include <randar/Engine/Resource.hpp>
 #include <randar/Scene/SceneModel.hpp>
 
 namespace randar
 {
-    class Scene
+    class Scene : virtual public Resource
     {
         /**
          * The desired frames per second in this scene.
