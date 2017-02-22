7913da83e35e59d31cd3d118a783339c38007433
Alexander Litty <github@alexlitty.com>
2017-02-21 16:33:47 -0800

    Removing old test suite.

--- src/Test/Filesystem/BinaryStream.cpp
+++
@@ -1,9 +0,0 @@
-#include <randar/Filesystem/Directory.hpp>
-#include <randar/Test/Test.hpp>
-
-TEST_CASE("binary streams")
-{
-    auto file = randar::Directory::Temp.getFile("randar.bin");
-
-    REQUIRE(1 == 1);
-}
--- src/Test/Test.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_RUNNER
-#include <randar/Test/Test.hpp>
-
-// Runs all available test suites.
-void randar::test()
-{
-    Catch::Session session;
-    session.run();
-}
