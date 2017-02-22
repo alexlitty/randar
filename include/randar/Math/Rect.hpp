24eea326bed8b4a965195fa91d275fb219732481
Alexander Litty <github@alexlitty.com>
2017-02-22 00:19:20 -0800

    Constructing Rect from CefRect.

--- include/randar/Math/Rect.hpp
+++ include/randar/Math/Rect.hpp
@@ -1,21 +1,38 @@
 #ifndef RANDAR_MATH_RECT_HPP
 #define RANDAR_MATH_RECT_HPP
 
+#include <cef/include/internal/cef_types_wrappers.h>
+
 namespace randar
 {
     template <typename T>
     struct Rect
     {
-        T top;
         T left;
+        T top;
         T width;
         T height;
 
-        Rect(T initTop, T initLeft, T initWidth, T initHeight)
-        : top(initTop),
-          left(initLeft),
+        /**
+         * Constructor.
+         */
+        Rect(T initLeft, T initTop, T initWidth, T initHeight)
+        : left(initLeft),
+          top(initTop),
           width(initWidth),
           height(initHeight)
+        {
+
+        }
+
+        /**
+         * Construct from a CEF rect.
+         */
+        Rect(const ::CefRect& rect)
+        : left(rect.x),
+          top(rect.y),
+          width(rect.width),
+          height(rect.height)
         {
 
         }
