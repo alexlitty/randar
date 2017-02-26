c64f89b09f078ac2aeea94a379228fb235cd90fb
Alexander Litty <github@alexlitty.com>
2017-02-26 03:03:16 -0800

    New Cursor enum.

---
+++ include/randar/Ui/Cursor.hpp
@@ -0,0 +1,14 @@
+#ifndef RANDAR_UI_CURSOR_HPP
+#define RANDAR_UI_CURSOR_HPP
+
+namespace randar
+{
+    enum class Cursor
+    {
+        NORMAL,
+        POINTER,
+        TEXT
+    };
+}
+
+#endif
