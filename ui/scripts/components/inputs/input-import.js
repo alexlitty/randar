5fad248a19c2508d46e136773fe1f4cd497bd2b8
Alexander Litty <github@alexlitty.com>
2017-02-28 22:53:39 -0800

    Importing works once more.

--- ui/scripts/components/inputs/input-import.js
+++ ui/scripts/components/inputs/input-import.js
@@ -1,8 +1,16 @@
 (function() {
     randar.component('input-import',
         {
+            methods: {
+                onClick: function() {
+                    window.importResource();
+                }
+            },
+
             template: `
-                <div class="button import">Import Resource</div>
+                <div class="button import" @click="onClick">
+                    Import Resource
+                </div>
             `
         }
     );
