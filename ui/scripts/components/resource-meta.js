657eb82a534a545a5a98d73f54331144175a973c
Alexander Litty <github@alexlitty.com>
2017-02-28 21:23:20 -0800

    Moving resource-meta to separate file.

---
+++ ui/scripts/components/resource-meta.js
@@ -0,0 +1,54 @@
+(function() {
+    var component = require('./component');
+
+    /**
+     * A table of resource metadata.
+     */
+    module.exports = component('resource-meta',
+        {
+            props: {
+                resource: Object
+            },
+
+            computed: {
+                metaProps: function() {
+                    if (this.resource.category === 'models') {
+                        return {
+                            vertexCount: 'Vertex Count',
+                            faceCount: 'Face Count'
+                        };
+                    }
+
+                    else if (this.resource.category === 'textures') {
+                        return {
+                            type: 'Type',
+                            width: 'Width',
+                            height: 'Height'
+                        };
+                    }
+
+                    else {
+                        return { };
+                    }
+                }
+            },
+
+            template: `
+                <table class="metadata">
+                    <thead>
+                        <tr>
+                            <th colspan="2">{{ resource.name }}</th>
+                        </tr>
+                    </thead>
+
+                    <tbody v-if="_.size(metaProps)">
+                        <tr v-for="(propTitle, propId) in metaProps">
+                            <th>{{ propTitle }}</th>
+                            <td>{{ resource[propId] }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            `
+        }
+    );
+})();
--- ui/scripts/ui-components.js
+++ ui/scripts/ui-components.js
@@ -55,55 +55,6 @@ var Component = { };
 Component.Common = require('./components/common');
 
 /**
- * A table of resource metadata.
- */
-Vue.component('resource-meta', {
-    props: {
-        resource: Object
-    },
-
-    computed: {
-        metaProps: function() {
-            if (this.resource.category === 'models') {
-                return {
-                    vertexCount: 'Vertex Count',
-                    faceCount: 'Face Count'
-                };
-            }
-
-            else if (this.resource.category === 'textures') {
-                return {
-                    type: 'Type',
-                    width: 'Width',
-                    height: 'Height'
-                };
-            }
-
-            else {
-                return { };
-            }
-        }
-    },
-
-    template: `
-        <table class="metadata">
-            <thead>
-                <tr>
-                    <th colspan="2">{{ resource.name }}</th>
-                </tr>
-            </thead>
-
-            <tbody v-if="_.size(metaProps)">
-                <tr v-for="(propTitle, propId) in metaProps">
-                    <th>{{ propTitle }}</th>
-                    <td>{{ resource[propId] }}</td>
-                </tr>
-            </tbody>
-        </table>
-    `
-});
-
-/**
  * An editor for models.
  */
 Vue.component('editor-model', combine(
