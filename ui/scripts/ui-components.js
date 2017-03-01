758623d2ebfbb84d6da0e177f818c76a320f651c
Alexander Litty <github@alexlitty.com>
2017-02-28 21:17:23 -0800

    Moving target-resource-panel to separate file.

--- ui/scripts/ui-components.js
+++ ui/scripts/ui-components.js
@@ -17,6 +17,10 @@ global.combine = function() {
 
     var result = { };
     for (component of arguments) {
+        if (!component) {
+            continue;
+        }
+
         if (component.data) {
             result.data = component.data;
         }
@@ -49,39 +53,6 @@ var Component = { };
  * A generic component for common functionality.
  */
 Component.Common = require('./components/common');
-
-/**
- * A panel to interact with the currently targeted resource.
- */
-Component.TargetResourcePanel = combine(
-    Component.Common,
-    {
-        computed: {
-            resource: function() {
-                var result = this.getSelectedResource();
-                result.category = _.clone(this.target.resource.category);
-                result.hasTextures = !!(result.textures && result.textures.length);
-                return result;
-            }
-        },
-
-        template: `
-            <div v-show="isResourceSelected()">
-                <nav id="target-resource">
-                    <back-button :action="clearTargetResource" />
-
-                    <resource-meta :resource="resource" />
-                    <editor-model v-if="resource.category === 'models'" :model="resource" />
-                </nav>
-
-                <section>
-                    <scene-timeline v-if="resource.category === 'scenes'">
-                    </scene-timeline>
-                </section>
-            </div>
-        `
-    }
-);
 
 /**
  * A marker on a timeline.
