aea7dda6daf0cd59173432f404dac47b96b2883c
Alexander Litty <github@alexlitty.com>
2017-03-02 00:01:54 -0800

    New dialog-menu component.

---
+++ ui/scripts/dialogs/dialog-menu.js
@@ -0,0 +1,11 @@
+(function() {
+    randar.component('dialog-menu', {
+        template: `
+            <menu class="dialog">
+                <menuitem>Randar</menuitem>
+                <menuitem>Another</menuitem>
+                <menuitem>About Item</menuitem>
+            </menu>
+        `
+    });
+})();
