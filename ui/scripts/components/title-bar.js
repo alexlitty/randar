71f01558884127ec30f7313ee8618f81a6edebc0
Alexander Litty <github@alexlitty.com>
2017-03-02 00:02:36 -0800

    Introducing a title bar for the program.

---
+++ ui/scripts/components/title-bar.js
@@ -0,0 +1,9 @@
+(function() {
+    randar.component('title-bar', {
+        template: `
+            <div id="title-bar">
+                <dialog-menu></dialog-menu>
+            </div>
+        `
+    });
+})();
