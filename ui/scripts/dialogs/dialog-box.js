485b3c4839483013691a045d4386e7395064b504
Alexander Litty <github@alexlitty.com>
2017-03-03 01:53:51 -0800

    Introducing a dialog box component.

---
+++ ui/scripts/dialogs/dialog-box.js
@@ -0,0 +1,9 @@
+(function() {
+    randar.component('dialog-box', {
+        template: `
+            <div class="draggable">
+                <slot></slot>
+            </div>
+        `
+    });
+})();
