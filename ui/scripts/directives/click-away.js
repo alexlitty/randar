f1ec19368c8dfcb79d73584137688a88a5113f1f
Alexander Litty <github@alexlitty.com>
2017-03-02 23:30:54 -0800

    click-away properly removes click listener.

--- ui/scripts/directives/click-away.js
+++ ui/scripts/directives/click-away.js
@@ -1,15 +1,19 @@
 (function() {
     Vue.directive('click-away', {
         bind: function(el, binding, vnode) {
-            this.event = function(event) {
+            el.clickAwayFn = function(event) {
                 if (!(el == event.target || el.contains(event.target))) {
-                    vnode.context[binding.expression](event);
+                    vnode.context[binding.expression]();
                 }
             }
+
+            setTimeout(function() {
+                document.body.addEventListener('click', el.clickAwayFn);
+            }, 0);
         },
 
         unbind: function(el) {
-            document.body.removeEventListener('click', this.event);
+            document.body.removeEventListener('click', el.clickAwayFn);
         }
     });
 })();
