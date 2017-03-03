618a12799b66e7618edfa49d53327062f6a9423c
Alexander Litty <github@alexlitty.com>
2017-03-02 20:53:14 -0800

    Adding context menus to menu bar items.

--- ui/scripts/bars/menu-bar.js
+++ ui/scripts/bars/menu-bar.js
@@ -5,10 +5,27 @@
             onClose: Function
         },
 
+        methods: {
+            onItemClick: function(item) {
+                if (item.contextMenu) {
+                    item.active = true;
+                }
+
+                if (item.onClick) {
+                    item.onClick();
+                }
+            }
+        },
+
         template: `
             <menu class="bar">
-                <menuitem v-for="item in items">
+                <menuitem v-for="item in items" @click="onItemClick(item)">
                     {{ item.text }}
+
+                    <context-menu v-if="item.contextMenu && item.active"
+                     v-click-away="item.active = false"
+                     :groups="item.contextMenu">
+                    </context-menu>
                 </menuitem>
 
                 <menuitem class="close" v-if="onClose" @click="onClose">
