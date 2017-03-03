3b1a5beed24a7dc1402a8512e8afc95ebc4113c3
Alexander Litty <github@alexlitty.com>
2017-03-03 00:09:50 -0800

    Clicking on items executes their actions.

--- ui/scripts/dialogs/context-menu.js
+++ ui/scripts/dialogs/context-menu.js
@@ -7,13 +7,21 @@
         methods: {
             onClose: function() {
                 this.$emit('close');
+            },
+
+            onItemClick: function(item) {
+                this.$emit('close');
+
+                if (item.action) {
+                    item.action();
+                }
             }
         },
 
         template: `
             <menu class="context" v-click-away="onClose">
                 <menu class="group" v-for="group in groups">
-                    <menuitem v-for="item in group">
+                    <menuitem v-for="item in group" @click="onItemClick(item)">
                         {{ item.text }}
                     </menuitem>
                 </menu>
