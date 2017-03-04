b51356e78668c3ccf71b40834367a8d8cb8a5e44
Alexander Litty <github@alexlitty.com>
2017-03-03 21:43:42 -0800

    Adding default resources to master bin for now.

--- ui/scripts/components/dialogs/bin-dialog.js
+++ ui/scripts/components/dialogs/bin-dialog.js
@@ -1,25 +1,27 @@
 (function() {
     randar.component('bin-dialog', {
-        data: function () {
-            return {
-                items: [
-                    { text: 'Master bin' }
-                ],
-
-                objects: [
-                    _.mapObject(this.textures, function(texture, textureId) {
-                        return {
-                            id   : textureId,
-                            text : texture.name
-                        };
-                    })
-                ]
+        computed: {
+            bin: function() {
+                return [
+                    { name: 'Scenes', items: this.scenes },
+                    { name: 'Models', items: this.models },
+                    { name: 'Textures', items: this.textures }
+                ];
             }
         },
 
         template: `
             <dialog-box>
                 <header class="drag-handle">Master bin</header>
+
+                <div class="bin">
+                    <div class="folder" v-for="folder in bin">
+                        <div class="name">{{ folder.name }}</div>
+                        <div class="item" v-for="item in folder.items">
+                            {{ item.name }}
+                        </div>
+                    </div>
+                </div>
             </dialog-box>
         `
     });
