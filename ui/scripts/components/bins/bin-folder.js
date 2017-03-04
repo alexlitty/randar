c481ab32bc001540d7e3b27bb5782b86807519bc
Alexander Litty <github@alexlitty.com>
2017-03-04 01:25:31 -0800

    Toggleable expanding on folders.

--- ui/scripts/components/bins/bin-folder.js
+++ ui/scripts/components/bins/bin-folder.js
@@ -1,22 +1,37 @@
 (function() {
     randar.component('bin-folder', {
         props: {
-            root     : Boolean,
-            contents : Object
+            root   : Boolean,
+            folder : Object
+        },
+
+        data: function() {
+            return {
+                expanded: this.root || !!this.folder.expanded
+            };
+        },
+
+        methods: {
         },
 
         template: `
             <div class="folder">
-                <div class="name" v-if="!root">{{ contents.name }}</div>
+                <div v-if="!root"
+                 :class="['name', expanded ? 'expanded' : null]"
+                 @click="expanded = !expanded">
+                    {{ folder.name }}
+                </div>
 
-                <bin-folder v-for="(folder, folderId) in contents.folders"
-                 :key="folderId"
-                 :contents="folder">
-                </bin-folder>
+                <div v-if="expanded">
+                    <bin-folder v-for="(subfolder, subfolderId) in folder.folders"
+                     :key="subfolderId"
+                     :folder="subfolder">
+                    </bin-folder>
 
-                <div class="item" v-for="(item, itemId) in contents.items"
-                 :key="itemId">
-                    <div class="name">{{ item.name }}</div>
+                    <div class="item" v-for="(item, itemId) in folder.items"
+                     :key="itemId">
+                        <div class="name">{{ item.name }}</div>
+                    </div>
                 </div>
             </div>
         `
