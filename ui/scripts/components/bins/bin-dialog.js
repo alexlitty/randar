1fd00302168a6e172ab272e9b8611d3f41a1c306
Alexander Litty <github@alexlitty.com>
2017-03-04 01:25:11 -0800

    Adding expanded property to folders.

--- ui/scripts/components/bins/bin-dialog.js
+++ ui/scripts/components/bins/bin-dialog.js
@@ -5,9 +5,9 @@
                 return {
                     name    : 'Master bin',
                     folders : [
-                        { name: 'Scenes', items: _.toArray(this.scenes) },
-                        { name: 'Models', items: _.toArray(this.models) },
-                        { name: 'Textures', items: _.toArray(this.textures) }
+                        { name: 'Scenes', items: _.toArray(this.scenes), expanded: false },
+                        { name: 'Models', items: _.toArray(this.models), expanded: false },
+                        { name: 'Textures', items: _.toArray(this.textures), expanded: false }
                     ]
                 }
             }
@@ -17,7 +17,7 @@
             <dialog-box>
                 <header class="drag-handle">Master bin</header>
 
-                <bin-folder class="bin" :contents="bin" :root="true">
+                <bin-folder class="bin" :folder="bin" :root="true">
                 </bin-folder>
             </dialog-box>
         `
