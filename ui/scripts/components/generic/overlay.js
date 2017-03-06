d4ada633db5867bb12832bdff7afd40ffc165924
Alexander Litty <github@alexlitty.com>
2017-03-05 21:22:34 -0800

    Automatically hiding overlay on click-away.

--- ui/scripts/components/generic/overlay.js
+++ ui/scripts/components/generic/overlay.js
@@ -1,8 +1,18 @@
 (function() {
     randar.component('overlay', {
+        props: {
+            name: String
+        },
+
+        methods: {
+            hideOverlay: function() {
+                this.$set(this.overlays, this.name, false);
+            }
+        },
+
         template: `
-            <div class="overlay">
-                <div class="content">
+            <div class="overlay" v-if="overlays[name]">
+                <div class="content" v-click-away="hideOverlay">
                     <slot></slot>
                 </div>
             </div>
