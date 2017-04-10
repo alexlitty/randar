85d1b411ce61573e9be1e3c80ef61c926637fb4d
Alexander Litty <github@alexlitty.com>
2017-04-10 00:05:33 -0700

    Moving engine back up as a module.

--- _v1/ui/scripts/components/generic/dialog-box.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    randar.component('dialog-box', {
-        template: `
-            <div class="dialog-box draggable">
-                <slot></slot>
-            </div>
-        `
-    });
-})();
--- _v1/ui/scripts/filters/titlecase.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    Vue.filter('titlecase', function(value) {
-        if (!_.isString(value) || !value.length) {
-            return value;
-        }
-
-        return value.charAt(0).toUpperCase() + value.slice(1);
-    });
-})();
