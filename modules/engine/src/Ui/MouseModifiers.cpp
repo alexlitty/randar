99e86ef1eee080ef58df811cd5f2f6908cfa3908
Alexander Litty <github@alexlitty.com>
2017-04-11 03:36:42 -0700

    Removing old CEF code.

--- modules/engine/src/Ui/MouseModifiers.cpp
+++
@@ -1,8 +0,0 @@
-#include <randar/Ui/MouseModifiers.hpp>
-
-void randar::MouseModifiers::set(int modifiers)
-{
-    this->shift = modifiers && GLFW_MOD_SHIFT;
-    this->control = modifiers && GLFW_MOD_CONTROL;
-    this->alt = modifiers && GLFW_MOD_ALT;
-}
