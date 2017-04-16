c43f45695b757069930ae77bbc7142c20f1fd448
Alexander Litty <github@alexlitty.com>
2017-04-16 01:32:18 -0700

    Improving Color class.

--- modules/engine/include/randar/Render/Color.hpp
+++ modules/engine/include/randar/Render/Color.hpp
@@ -1,19 +1,148 @@
 #ifndef RANDAR_RENDER_COLOR_HPP
 #define RANDAR_RENDER_COLOR_HPP
 
+#include <cstdint>
+
 namespace randar
 {
-    struct Color
+    /**
+     * A color represented by red, green, blue, and alpha channels.
+     *
+     * The alpha channel represents complete transparency at value 0.
+     *
+     * Channels can be set and retrieved via 8-bit integers, but the
+     * functionality is only provided for convenience. Don't use the integer
+     * interfaces for core code.
+     */
+    class Color
     {
-        float r;
-        float g;
-        float b;
-        float a;
+        /**
+         * Internal data for this color.
+         *
+         * The four elements represent the red, green, blue, and alpha channels
+         * respectively.
+         *
+         * Each value is ideally clamped within [0, 1].
+         */
+        float data[4];
 
+    public:
+        /**
+         * Default constructor.
+         *
+         * Color is initialized to opaque black.
+         */
         Color();
+
+        /**
+         * Primary constructor.
+         *
+         * Delegates to set, which safely initializes the color using floats.
+         * Values are clamped within [0, 1].
+         *
+         * Alpha is optional and defaults to 1, fully opaque.
+         *
+         * No constructor is provided for blindly initializing the color values.
+         * If performance is required, consider calling _set on an existing
+         * instance or not using the class altogether.
+         *
+         * No constructor is provided for initializing the color using 8-bit
+         * integers, for intuitive Node.js compatibility.
+         *
+         */
         Color(float rInit, float gInit, float bInit, float aInit = 1.0f);
+
+        /**
+         * Safely sets the color using float values.
+         *
+         * Values are clamped within [0, 1]. If all values are always clamped
+         * beforehand, use _set.
+         *
+         * Alpha is optional and defaults to 1, fully opaque.
+         */
+        void set(float rNew, float gNew, float bNew, float aNew = 1.0f);
+
+        /**
+         * Blindly sets the color using float values.
+         *
+         * If any value is not clamped within [0, 1], behavior is undefined. If
+         * any value may not be clamped beforehand, use set.
+         *
+         * Alpha is optional and defaults to 1, fully opaque.
+         */
+        inline void _set(float rNew, float gNew, float bNew, float aNew = 1.0f)
+        {
+            this->data[0] = rNew;
+            this->data[1] = gNew;
+            this->data[2] = bNew;
+            this->data[3] = aNew;
+        }
+
+        /**
+         * Safely sets a channel on the color using float values.
+         *
+         * Values are clamped within [0, 1].
+         */
+        void r(float v);
+        void g(float v);
+        void b(float v);
+        void a(float v);
+
+        /**
+         * Blindly sets a channel on the color using float values.
+         *
+         * If a value is not clamped within [0, 1], behavior is undefined. If
+         * any value may not be clamped beforehand, use the safe method
+         * alternative.
+         */
+        inline void _r(float v) { this->data[0] = v; }
+        inline void _g(float v) { this->data[1] = v; }
+        inline void _b(float v) { this->data[2] = v; }
+        inline void _a(float v) { this->data[3] = v; }
+
+        /**
+         * Sets the color using 8-bit integer values.
+         *
+         * Alpha is optional and defaults to 255, fully opaque.
+         */
+        void setInt(uint8_t rNew, uint8_t gNew, uint8_t bNew, uint8_t aNew = 255);
+
+        /**
+         * Sets a channel on the color using 8-bit integer values.
+         */
+        void rInt(uint8_t v);
+        void gInt(uint8_t v);
+        void bInt(uint8_t v);
+        void aInt(uint8_t v);
+
+        /**
+         * Gets a channel on the color as a float clamped within [0, 1].
+         */
+        float r() const;
+        float g() const;
+        float b() const;
+        float a() const;
+
+        /**
+         * Gets a channel on the color as an 8-bit integer.
+         */
+        uint8_t rInt() const;
+        uint8_t gInt() const;
+        uint8_t bInt() const;
+        uint8_t aInt() const;
+
+        /**
+         * Randomizes the red, green, and blue channels in this color.
+         *
+         * If randomizeAlpha is true the alpha channel is also randomized,
+         * otherwise it is left unaltered. By default randomizeAlpha is false.
+         */
+        void randomize(bool randomizeAlpha = false);
     };
 
+    /**
+     * Generates a random color with full opacity.
+     */
     Color randomColor();
 }
 
--- modules/engine/src/Render/Color.cpp
+++ modules/engine/src/Render/Color.cpp
@@ -1,26 +1,135 @@
 #include <randar/Math/Random.hpp>
 #include <randar/Render/Color.hpp>
+#include <randar/Utility/Clamp.hpp>
 
+// Default constructor.
 randar::Color::Color()
-: randar::Color::Color(1.0f, 1.0f, 1.0f)
+: randar::Color::Color(0.0f, 0.0f, 0.0f)
 {
 
 }
 
+// Primary constructor.
 randar::Color::Color(float rInit, float gInit, float bInit, float aInit)
-: r(rInit),
-  g(gInit),
-  b(bInit),
-  a(aInit)
 {
-
+    this->set(rInit, gInit,  bInit, aInit);
 }
 
+// Safely sets the color using float values.
+void randar::Color::set(float rNew, float gNew, float bNew, float aNew)
+{
+    this->r(rNew);
+    this->g(gNew);
+    this->b(bNew);
+    this->a(aNew);
+}
+
+// Safely sets a channel on the color using float values.
+void randar::Color::r(float v)
+{
+    this->_r(randar::clampUnsignedFloat(v));
+}
+
+void randar::Color::g(float v)
+{
+    this->_g(randar::clampUnsignedFloat(v));
+}
+
+void randar::Color::b(float v)
+{
+    this->_b(randar::clampUnsignedFloat(v));
+}
+
+void randar::Color::a(float v)
+{
+    this->_a(randar::clampUnsignedFloat(v));
+}
+
+// Sets the color using 8-bit integer values.
+void randar::Color::setInt(uint8_t rNew, uint8_t gNew, uint8_t bNew, uint8_t aNew)
+{
+    this->rInt(rNew);
+    this->bInt(bNew);
+    this->gInt(gNew);
+    this->aInt(aNew);
+}
+
+// Sets a channel on the color using 8-bit integer values.
+void randar::Color::rInt(uint8_t v)
+{
+    this->_r(v / 255.0f);
+}
+
+void randar::Color::gInt(uint8_t v)
+{
+    this->_g(v / 255.0f);
+}
+
+void randar::Color::bInt(uint8_t v)
+{
+    this->_b(v / 255.0f);
+}
+
+void randar::Color::aInt(uint8_t v)
+{
+    this->_a(v / 255.0f);
+}
+
+// Gets a channel on the color as a float clamped within [0, 1].
+float randar::Color::r() const
+{
+    return this->data[0];
+}
+
+float randar::Color::b() const
+{
+    return this->data[1];
+}
+
+float randar::Color::g() const
+{
+    return this->data[2];
+}
+
+float randar::Color::a() const
+{
+    return this->data[3];
+}
+
+// Gets a channel on the color as an 8-bit integer.
+uint8_t randar::Color::rInt() const
+{
+    return this->r() * 255.0f;
+}
+
+uint8_t randar::Color::gInt() const
+{
+    return this->g() * 255.0f;
+}
+
+uint8_t randar::Color::bInt() const
+{
+    return this->b() * 255.0f;
+}
+
+uint8_t randar::Color::aInt() const
+{
+    return this->a() * 255.0f;
+}
+
+// Randomizes the red, green, and blue channels in this color.
+void randar::Color::randomize(bool randomizeAlpha)
+{
+    this->rInt(randar::randomFloat());
+    this->gInt(randar::randomFloat());
+    this->bInt(randar::randomFloat());
+    this->aInt(randar::randomFloat());
+}
+
+// Generates a random color with full opacity.
 randar::Color randar::randomColor()
 {
-    return Color(
-        randar::random(0, 255) / 255.0f,
-        randar::random(0, 255) / 255.0f,
-        randar::random(0, 255) / 255.0f
-    );
+    Color color;
+    color.randomize();
+    return color;
 }
