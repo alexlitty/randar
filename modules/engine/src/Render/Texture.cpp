7f89c6a65ab81a9ac243388533bf4e08c21517e1
Alexander Litty <github@alexlitty.com>
2017-04-16 01:35:07 -0700

    Making textures compatible with new Color class.

--- modules/engine/src/Render/Texture.cpp
+++ modules/engine/src/Render/Texture.cpp
@@ -84,10 +84,10 @@ void randar::Texture::clear(const Color& color)
 
     uint32_t pixelCount = this->width * this->height;
     for (unsigned int i = 0; i < pixelCount; i++) {
-        values.push_back(color.r);
-        values.push_back(color.g);
-        values.push_back(color.b);
-        values.push_back(color.a);
+        values.push_back(color.r());
+        values.push_back(color.g());
+        values.push_back(color.b());
+        values.push_back(color.a());
     }
 
     this->gpu.write(*this, values.data(), GL_RGBA);
