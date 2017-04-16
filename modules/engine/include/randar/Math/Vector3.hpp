0b2c38926c6feeb936421f273c19f9d143b91338
Alexander Litty <github@alexlitty.com>
2017-04-16 01:43:19 -0700

    Renaming Vector to Vector3.

---
+++ modules/engine/include/randar/Math/Vector3.hpp
@@ -0,0 +1,90 @@
+#ifndef RANDAR_MATH_VECTOR_HPP
+#define RANDAR_MATH_VECTOR_HPP
+
+#include <string>
+#include <bullet3/btBulletDynamicsCommon.h>
+#include <randar/Utility/glm.hpp>
+#include <randar/Utility/Json.hpp>
+
+namespace randar
+{
+    struct Vector3
+    {
+        float x;
+        float y;
+        float z;
+
+        Vector3();
+        Vector3(const btVector33& other);
+        Vector3(float xInit, float yInit, float zInit = 0.0f);
+        void set(float xNew, float yNew, float zNew = 0.0f);
+
+        /**
+         * Normalizes this vector.
+         */
+        void normalize();
+        Vector3 normalized() const;
+
+        /**
+         * Calculates the magnitude of this vector.
+         */
+        float getMagnitude() const;
+
+        /**
+         * Dot and cross products.
+         */
+        float dot(Vector3 other) const;
+        Vector3 cross(Vector3 other) const;
+
+        /**
+         * Transforms this vector.
+         */
+        void transform(const glm::mat4& matrix);
+
+        /**
+         * Transformation assignment operator.
+         */
+        Vector3& operator *=(const glm::mat4& matrix);
+
+        /**
+         * Mathematical assignment operators.
+         */
+        Vector3& operator +=(const Vector3& other);
+        Vector3& operator -=(const Vector3& other);
+        Vector3& operator *=(float other);
+        Vector3& operator /=(float other);
+
+        /**
+         * Convert to physics vector.
+         */
+        operator btVector33() const;
+
+        /**
+         * Convert to string.
+         */
+        std::string toString() const;
+        operator std::string() const;
+
+        /**
+         * Convert to JSON.
+         */
+        Json toJson() const;
+    };
+
+    /**
+     * Transformation operators.
+     */
+    Vector3 operator *(Vector3 vector, const glm::mat4& matrix);
+    Vector3 operator *(const glm::mat4& matrix, Vector3 vector);
+
+    /**
+     * Mathematical operators.
+     */
+    Vector3 operator +(Vector3 lhs, const Vector3& rhs);
+    Vector3 operator -(Vector3 lhs, const Vector3& rhs);
+    Vector3 operator *(Vector3 lhs, float rhs);
+    Vector3 operator *(float lhs, Vector3 rhs);
+    Vector3 operator /(Vector3 lhs, float other);
+}
+
+#endif
---
+++ modules/engine/src/Math/Vector3.cpp
@@ -0,0 +1,192 @@
+#include <randar/Math/Vector3.hpp>
+
+randar::Vector3::Vector3()
+: randar::Vector3::Vector3(0.0f, 0.0f, 0.0f)
+{
+
+}
+
+randar::Vector3::Vector3(const btVector33& other)
+: x(other.x()),
+  y(other.y()),
+  z(other.z())
+{
+
+}
+
+randar::Vector3::Vector3(float xInit, float yInit, float zInit)
+: x(xInit),
+  y(yInit),
+  z(zInit)
+{
+
+}
+
+void randar::Vector3::set(float xNew, float yNew, float zNew)
+{
+    this->x = xNew;
+    this->y = yNew;
+    this->z = zNew;
+}
+
+// Normalizes this vector.
+void randar::Vector3::normalize()
+{
+    float magnitude = this->getMagnitude();
+    if (!magnitude) {
+        this->x = this->y = this->z = 0.0f;
+    }
+    this->x /= magnitude;
+    this->y /= magnitude;
+    this->z /= magnitude;
+}
+
+randar::Vector3 randar::Vector3::normalized() const
+{
+    Vector3 result(*this);
+    result.normalize();
+    return result;
+}
+
+// Calculates the magnitude of this vector.
+float randar::Vector3::getMagnitude() const
+{
+    return std::sqrt(
+          (this->x * this->x)
+        + (this->y * this->y)
+        + (this->z * this->z)
+    );
+}
+
+// Dot and cross products.
+float randar::Vector3::dot(randar::Vector3 other) const
+{
+    return (this->x * other.x) + (this->y * other.y) + (this->z * other.z);
+}
+
+randar::Vector3 randar::Vector3::cross(randar::Vector3 other) const
+{
+    other.set(
+        (this->y * other.z) - (this->z * other.y),
+        (this->z * other.x) - (this->x * other.z),
+        (this->x * other.y) - (this->y * other.x)
+    );
+
+    return other;
+}
+
+// Transforms this vector.
+void randar::Vector3::transform(const glm::mat4& matrix)
+{
+    glm::vec4 result(this->x, this->y, this->z, 1.0f);
+    result = matrix * result;
+    this->set(result.x, result.y, result.z);
+}
+
+// Transformation operators.
+randar::Vector3& randar::Vector3::operator *=(const glm::mat4& matrix)
+{
+    this->transform(matrix);
+    return *this;
+}
+
+randar::Vector3 randar::operator *(randar::Vector3 vector, const glm::mat4& matrix)
+{
+    return vector *= matrix;
+}
+
+randar::Vector3 randar::operator *(const glm::mat4& matrix, randar::Vector3 vector)
+{
+    return vector * matrix;
+}
+
+// Addition operators.
+randar::Vector3& randar::Vector3::operator +=(const Vector3& other)
+{
+    this->x += other.x;
+    this->y += other.y;
+    this->z += other.z;
+    return *this;
+}
+
+randar::Vector3 randar::operator +(randar::Vector3 lhs, const randar::Vector3& rhs)
+{
+    return lhs += rhs;
+}
+
+// Subtraction operators.
+randar::Vector3& randar::Vector3::operator -=(const randar::Vector3& other)
+{
+    this->x -= other.x;
+    this->y -= other.y;
+    this->z -= other.z;
+    return *this;
+}
+
+randar::Vector3 randar::operator -(randar::Vector3 lhs, const randar::Vector3& rhs)
+{
+    return lhs -= rhs;
+}
+
+// Multiplication operators.
+randar::Vector3& randar::Vector3::operator *=(float other)
+{
+    this->x *= other;
+    this->y *= other;
+    this->z *= other;
+    return *this;
+}
+
+randar::Vector3 randar::operator *(randar::Vector3 lhs, float rhs)
+{
+    return lhs *= rhs;
+}
+
+randar::Vector3 randar::operator *(float lhs, randar::Vector3 rhs)
+{
+    return rhs * lhs;
+}
+
+// Division operators.
+randar::Vector3& randar::Vector3::operator /=(float other)
+{
+    this->x /= other;
+    this->y /= other;
+    this->z /= other;
+    return *this;
+}
+
+randar::Vector3 randar::operator /(randar::Vector3 lhs, float rhs)
+{
+    return lhs /= rhs;
+}
+
+// Convert to physics vector.
+randar::Vector3::operator btVector33() const
+{
+    return btVector33(this->x, this->y, this->z);
+}
+
+// Convert to string.
+std::string randar::Vector3::toString() const
+{
+    return "("
+         + std::to_string(x) + ", "
+         + std::to_string(y) + ", "
+         + std::to_string(z) + ")";
+}
+
+randar::Vector3::operator std::string() const
+{
+    return this->toString();
+}
+
+// Convert to JSON.
+Json randar::Vector3::toJson() const
+{
+    return {
+        { "x", this->x },
+        { "y", this->y },
+        { "z", this->z }
+    };
+}
