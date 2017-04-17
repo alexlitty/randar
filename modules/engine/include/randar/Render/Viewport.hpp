7d4465b023cf20a2bd9a613c4f5cb36bd51721bb
Alexander Litty <github@alexlitty.com>
2017-04-17 00:29:57 -0700

    Improving Viewport initialization.

--- modules/engine/include/randar/Render/Viewport.hpp
+++ modules/engine/include/randar/Render/Viewport.hpp
@@ -11,7 +11,8 @@ namespace randar
         int x2;
         int y2;
 
-        Viewport(int ix1 = 0, int iy1 = 0, int ix2 = 1, int iy2 = 1);
+        Viewport();
+        Viewport(int ix1, int iy1, int ix2, int iy2);
         void set(int ix1, int iy1, int ix2, int iy2);
     };
 }
--- modules/engine/src/Render/Viewport.cpp
+++ modules/engine/src/Render/Viewport.cpp
@@ -1,5 +1,14 @@
 #include <randar/Render/Viewport.hpp>
 
+randar::Viewport::Viewport()
+: x1(0),
+  y1(0),
+  x2(1),
+  y2(1)
+{
+
+}
+
 randar::Viewport::Viewport(int ix1, int iy1, int ix2, int iy2)
 {
     this->set(ix1, iy1, ix2, iy2);
