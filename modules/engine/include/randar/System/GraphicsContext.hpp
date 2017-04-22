baba1b6561f4d96253326f141ce21e6140372837
Alexander Litty <github@alexlitty.com>
2017-04-22 01:33:51 -0700

    Fleshing out the new GraphicsContext.

--- modules/engine/include/randar/System/GraphicsContext.hpp
+++ modules/engine/include/randar/System/GraphicsContext.hpp
@@ -1,29 +1,83 @@
 #ifndef RANDAR_SYSTEM_GRAPHICS_CONTEXT_HPP
 #define RANDAR_SYSTEM_GRAPHICS_CONTEXT_HPP
 
+#include <stdexcept>
+#include <vector>
+#include <GL/glew.h>
+#include <GL/glx.h>
 #include <randar/Utility/Unassignable.hpp>
 
 namespace randar
 {
+    class Window;
+
     /**
-     * A simple wrapper for OpenGL contexts.
+     * A wrapper for OpenGL contexts.
      *
-     * Randar demands only one context may exist at any time for a single
-     * process. Any work that requires multiple contexts must be performed in
-     * separate processes.
+     * We use glew to map the correct gl calls to the desired OpenGL functions
+     * and GPU behavior. On environments leveraging X11 and GLX, glew handles
+     * multiple contexts seamlessly, as long as the contexts target the same
+     * OpenGL version. The gl functions in this case are not binded to a
+     * particular context, and will perform actions on the current context
+     * automatically.
      *
-     * If we allowed multiple contexts within a process, Randar would have to
-     * perform checks and context switches that hinder performance.
+     * On Windows in particular, glew must be re-initialized every time the
+     * current context is switched. Windows binds the gl functions to a specific
+     * context; A gl function created for one context only affects that context.
+     * Re-initializing glew binds the global gl functions to the desired
+     * context.
      *
-     * Any number of threads may share the process-wide context.
+     * As a result, this wraper is not compatible with Windows, and all contexts
+     * created within a program must target the same OpenGL version. For now.
+     *
+     * A simple solution is to call glewInit every time the current context
+     * changes. A performance-friendly solution is to save the gl functions
+     * associated with this context, and only call gl functions saved within
+     * this wrapper.
+     *
+     * This context, itself, does not expose a usable default framebuffer. To
+     * perform off-screen rendering, render to a new framebuffer that is
+     * associated with this context. To perform rendering in a window, use the
+     * default framebuffers exposed by randar Windows.
      */
     class GraphicsContext : public Unassignable
     {
         /**
-         * Constructor.
+         * Display that was used to construct this context.
+         */
+        ::Display *display;
+
+        /**
+         * Information about the visual used by this context.
+         */
+        ::XVisualInfo *visual;
+
+        /**
+         * Raw context this object wraps around.
+         */
+        ::GLXContext ctx;
+
+        /**
+         * GLX pixel buffer associated with this context.
          *
-         * Don't let this be called multiple times in a single process. Just
-         * call the static require method and everyone will be happy.
+         * Not to be confused with OpenGL's pixel buffer objects.
+         *
+         * This is created to initialize our OpenGL context in a way that
+         * enables windowless rendering. It is not used otherwise.
+         */
+        ::GLXPbuffer glxPixelBuffer;
+
+        /**
+         * Windows associated with this context, if any.
+         *
+         * We track these in case this context is destroyed before the windows
+         * are, in which case we also uninitialize the windows.
+         */
+        std::vector<randar::Window*> windows;
+
+    public:
+        /**
+         * Constructor.
          */
         GraphicsContext();
 
@@ -32,37 +86,32 @@ namespace randar
          */
         ~GraphicsContext();
 
-    public:
         /**
-         * Signals the requirement for a process-wide graphics context.
+         * Makes this context current without considering a window.
          *
-         * If the context is not yet initialized, it is initialized immediately.
-         * Nothing happens if the context is already initialized; It is safe to
-         * call this repeatedly.
-         *
-         * The context is assigned to randar::ctx.
+         * This simply enables off-screen rendering via framebuffers. To enable
+         * rendering to a window, make this context and the desired window
+         * current.
          */
-        static void require();
+        void use();
 
         /**
-         * Destroys the process-wide graphics context.
+         * Makes this context and a window current.
          *
-         * After destruction, randar::ctx is set to a nullptr.
+         * This enables rendering to the given window in addition to off-screen
+         * rendering.
          *
-         * You probably won't ever need to call this, but it's here for whatever
-         * evil plan you have.
+         * Essentially, this just allows you to render to the window's default
+         * framebuffer.
          */
-        static void destroy();
+        void use(randar::Window& window);
+
+        /**
+         * Allow Randar windows to manipulate the ongoing list of associated
+         * windows.
+         */
+        friend Window;
     };
-
-    /**
-     * Process-wide graphics context.
-     *
-     * When a context is needed, call GraphicsContext::require. Until that is
-     * called, ctx is a nullptr. If you ever need to destroy this, call
-     * GraphicsContext::destroy.
-     */
-    extern GraphicsContext* ctx;
 }
 
 #endif
--- modules/engine/src/System/GraphicsContext.cpp
+++ modules/engine/src/System/GraphicsContext.cpp
@@ -1,26 +1,151 @@
 #include <randar/System/GraphicsContext.hpp>
+#include <randar/System/Window.hpp>
+
+// Allows us to pass attributes while creating a context.
+typedef GLXContext (*glXCreateContextAttribsARBProc)(
+    Display*,
+    GLXFBConfig,
+    GLXContext,
+    Bool,
+    const int*
+);
 
 // Constructor.
 randar::GraphicsContext::GraphicsContext()
 {
+    // Load extensions.
+    glXCreateContextAttribsARBProc glXCreateContextAttribsARB =
+        (glXCreateContextAttribsARBProc)glXGetProcAddressARB(
+            (const GLubyte *) "glXCreateContextAttribsARB"
+        );
 
-}
-
-// Signals the requirement for a process-wide graphics context.
-void randar::GraphicsContext::require()
-{
-    if (!this->ctx) {
-        this->ctx = new randar::GraphicsContext;
+    if (!glXCreateContextAttribsARB) {
+        throw std::runtime_error("No support for GLX_ARB_create_context");
     }
-}
 
-// Destroys the process-wide graphics context.
-void randar::GraphicsContext::destroy()
-{
-    if (this->ctx) {
+    int fbAttribs[] = {
+        GLX_X_RENDERABLE, true,
+        GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
 
+        GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
+        GLX_RENDER_TYPE, GLX_RGBA_BIT,
+        GLX_DOUBLEBUFFER, false,
+
+        GLX_RED_SIZE, 8,
+        GLX_GREEN_SIZE, 8,
+        GLX_BLUE_SIZE, 8,
+        GLX_ALPHA_SIZE, 8,
+        GLX_ALPHA_SIZE, 8,
+        GLX_DEPTH_SIZE, 24,
+        GLX_STENCIL_SIZE, 8,
+        None
+    };
+
+    int pbAttribs[] = {
+        GLX_PBUFFER_WIDTH, 1,
+        GLX_PBUFFER_HEIGHT, 1,
+        None
+    };
+
+    int ctxAttribs[] = {
+        //GLX_CONTEXT_MAJOR_VERSION, 3,
+        //GLX_CONTEXT_MINOR_VERSION, 3,
+        None
+    };
+
+    // Get a local X display.
+    this->display = ::XOpenDisplay(0);
+
+    // Find a proper framebuffer configuration.
+    int fbCount = 0;
+    ::GLXFBConfig *fbConfigs = ::glXChooseFBConfig(
+        this->display,
+        DefaultScreen(this->display),
+        fbAttribs,
+        &fbCount
+    );
+
+    if (!fbConfigs || fbCount == 0) {
+        throw std::runtime_error("No proper framebuffers available");
     }
+
+    // Get the visual from our chosen framebuffer.
+    this->visual = ::glXGetVisualFromFBConfig(this->display, fbConfigs[0]);
+
+    // Create the GLX pixel buffer.
+    this->glxPixelBuffer = ::glXCreatePbuffer(
+        this->display,
+        fbConfigs[0],
+        pbAttribs
+    );
+
+    // Create the context.
+    this->ctx = glXCreateContextAttribsARB(
+        this->display,
+        fbConfigs[0],
+        0,
+        true,
+        ctxAttribs
+    );
+
+    // Wait for X to throw any errors available.
+    ::XSync(this->display, false);
+    ::XFree(fbConfigs);
+
+    // Immediately enable off-screen rendering.
+    this->use();
+
+    // Initialize glew.
+    ::glewExperimental = true;
+    GLenum status = ::glewInit();
+    if (status != GLEW_OK) {
+        throw std::runtime_error("Failed to initialize GLEW");
+    }
+
+    // Configure OpenGL.
+    ::glEnable(GL_VERTEX_ARRAY);
+    ::glEnable(GL_DEPTH_TEST);
+    ::glDepthFunc(GL_LESS);
+
+    ::glEnable(GL_BLEND);
+    ::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-// Process-wide graphics context.
-randar::GraphicsContext* ctx = nullptr;
+// Destructor.
+randar::GraphicsContext::~GraphicsContext()
+{
+    ::glXMakeCurrent(
+        this->display,
+        None,
+        None
+    );
+
+    for (auto window : this->windows) {
+        delete window;
+    }
+
+    ::glXDestroyPbuffer(this->display, this->glxPixelBuffer);
+    ::glXDestroyContext(this->display, this->ctx);
+    ::XFree(this->visual);
+    ::XCloseDisplay(this->display);
+}
+
+// Makes this context current without considering a window.
+void randar::GraphicsContext::use()
+{
+    ::glXMakeCurrent(
+        this->display,
+        this->glxPixelBuffer,
+        this->ctx
+    );
+}
+
+// Makes this context and a window current.
+void randar::GraphicsContext::use(randar::Window& window)
+{
+    ::glXMakeCurrent(
+        this->display,
+        window.glx(),
+        this->ctx
+    );
+}
