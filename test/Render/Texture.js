8e20927b14c96ec7bb34b5730a2f9bd195ef4141
Alexander Litty <github@alexlitty.com>
2017-04-23 00:16:16 -0700

    Adding Texture size tests.

--- test/Render/Texture.js
+++ test/Render/Texture.js
@@ -28,11 +28,37 @@ describe.only('Texture', function() {
 
             for (dim of dims) {
                 assertDims(
-                    new adapter.Texture(ctx, dim.x, dim.y),
+                    new adapter.Texture(ctx, dim.x, dim.y, 'rgba'),
                     dim.x,
                     dim.y
                 );
             }
+        });
+
+        it('refuses construction with invalid dimensions', function() {
+            assert.throws(() => new adapter.Texture(ctx, -32, 32, 'rgba'));
+            assert.throws(() => new adapter.Texture(ctx, 5000, 5000, 'rgba'));
+        });
+
+        it('resizes with correct dimensions', function() {
+            const dims = [
+                { x: 64, y: 64 },
+                { x: 128, y: 128 },
+                { x: 1024, y: 1024 }
+            ];
+
+            for (dim of dims) {
+                let texture = new adapter.Texture(ctx, 2, 2, 'rgba');
+
+                assert.doesNotThrow(() => texture.resize(dim.x, dim.y));
+                assertDims(texture, dim.x, dim.y);
+            }
+        });
+
+        it('refuses resizing with invalid dimensions', function() {
+            let texture = new adapter.Texture(ctx, 32, 32, 'rgba');
+            assert.throws(() => texture.resize(-32, 32));
+            assert.throws(() => texture.resize(5000, 5000));
         });
     });
 });
