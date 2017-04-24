6045f4128090a65feab77b35f9ebcba26f2f0741
Alexander Litty <github@alexlitty.com>
2017-04-23 22:26:06 -0700

    Bringing width & height functions into FB scope.

--- modules/engine/include/randar/Render/Framebuffer.hpp
+++ modules/engine/include/randar/Render/Framebuffer.hpp
@@ -35,6 +35,9 @@ namespace randar
         randar::Window *window;
 
     public:
+        using Dimensional2<uint32_t>::getWidth;
+        using Dimensional2<uint32_t>::getHeight;
+
         /**
          * Camera used to view this framebuffer.
          */
