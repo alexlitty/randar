d8d535e42d1ce5fccf65d971ec00eac2f0d64969
Alexander Litty <github@alexlitty.com>
2017-04-27 03:24:38 -0700

    Adding global variables in boot.js for tests.

--- test/boot.js
+++ test/boot.js
@@ -1,20 +1,25 @@
-const os       = require('os');
-const path     = require('path');
-const voidFile = os.platform() === 'win32' ? 'NUL' : '/dev/null';
+before(function() {
+    global.assert  = require('assert');
+    global.os      = require('os');
+    global.path    = require('path');
 
-/**
- * Register a segfault listener.
- *
- * This will display some hopefully-helpful information when a segfault occurs.
- * Which shouldn't happen, because our code is perfect, right? Right?!
- *
- * We specify /dev/null to 
- */
-global.SegfaultHandler = require('segfault-handler');
-SegfaultHandler.registerHandler(voidFile);
+    global.adapter = require(
+        path.join(__dirname, '..', 'modules', 'adapter', 'build/Debug/adapter.node')
+    );
 
-/**
- * Dummy Mocha function so this file is picked up while testing.
- */
-before(() => {
+    const voidFile = os.platform() === 'win32' ? 'NUL' : '/dev/null';
+
+    /**
+     * Register a segfault listener.
+     *
+     * This will display some hopefully-helpful information when a segfault occurs.
+     * Which shouldn't happen, because our code is perfect, right? Right?!
+     *
+     * registerHandler needs to be called to start listening for segfaults, and
+     * automatically logs the relevant information to the console. It takes a
+     * required filename argument to store the same information in; We don't really
+     * need to save the information, so we pass the platform's "void" file handle.
+     */
+    global.SegfaultHandler = require('segfault-handler');
+    SegfaultHandler.registerHandler(voidFile);
 });
