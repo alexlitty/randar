5a3f27fefeb7980697664d00ccbc7fc20e832b5b
Alexander Litty <github@alexlitty.com>
2017-04-28 22:01:39 -0700

    Using Release adapter module. boot.js comments.

--- test/boot.js
+++ test/boot.js
@@ -1,13 +1,14 @@
 before(function() {
+    /**
+     * Global declarations that will be available in all tests, even when
+     * exclusively run with Mocha's .only.
+     */
     global.assert  = require('assert');
     global.os      = require('os');
     global.path    = require('path');
 
-    global.adapter = require(
-        path.join(__dirname, '..', 'modules', 'adapter', 'build/Debug/adapter.node')
-    );
-
-    const voidFile = os.platform() === 'win32' ? 'NUL' : '/dev/null';
+    global.voidFile = os.platform() === 'win32' ? 'NUL' : '/dev/null';
+    global.adapter = require(path.join(__dirname, '..', 'modules', 'adapter'));
 
     /**
      * Register a segfault listener.
@@ -15,10 +16,15 @@ before(function() {
      * This will display some hopefully-helpful information when a segfault occurs.
      * Which shouldn't happen, because our code is perfect, right? Right?!
      *
-     * registerHandler needs to be called to start listening for segfaults, and
-     * automatically logs the relevant information to the console. It takes a
-     * required filename argument to store the same information in; We don't really
-     * need to save the information, so we pass the platform's "void" file handle.
+     * registerHandler needs to be called to start listening for segfaults. Once
+     * called, it automatically logs such events to the console. It takes a
+     * required filename argument to store the same information in; We don't
+     * really need to save the information, so we pass the platform's "void"
+     * file handle.
+     *
+     * You can configure the adapter to build as a debug module, but it doesn't
+     * seem to provide any additional information. The release module symbols
+     * appear descriptive enough for most problems anyway.
      */
     global.SegfaultHandler = require('segfault-handler');
     SegfaultHandler.registerHandler(voidFile);
