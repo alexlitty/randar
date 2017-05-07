0fe26a680fa71a75fc3d263cf6354eae5973d8a5
Alexander Litty <me@alexlitty.com>
2017-05-06 20:11:44 -0700

    Creating a config file with version info.

--- scripts/install-goals/node.js
+++ scripts/install-goals/node.js
@@ -1,13 +1,13 @@
-const spawn   = require('child_process').spawn;
-const helpers = require('../helpers');
+const spawn     = require('child_process').spawn;
+const helpers   = require('../helpers');
+const checkNode = require('../utility/check-node');
 
-if (process.version === helpers.requirements.node) {
-    console.log('Node.js is running at required version.');
+if (checkNode()) {
     process.exit(0);
 }
 
 console.log(
-    `Running Node.js ${process.version}. `
+    `Detected node.js ${process.version}. `
     + `Attempting to install required ${helpers.requirements.node}.`
 );
 
@@ -15,27 +15,23 @@ try {
     const opt = { stdio: 'inherit' };
 
     const install = spawn('sudo', ['npm', 'install', '-g', 'n'], opt);
-    console.log('Installing version manager "n"');
-
     install.on('close', (code) => {
         if (code !== 0) {
             throw new Error('Failed to install n.');
         }
 
         const use = spawn('sudo', ['n', helpers.requirements.node.slice(1)], opt);
-        console.log(`Requiring Node.js ${helpers.requirements.node}`);
-
         use.on('close', (code) => {
             if (code !== 0) {
                 throw new Error('Failed to install or switch Node.js version.');
             }
 
-            process.exit(1);
+            console.log(`Node.js ${helpers.requirements.node} installed.`);
         });
     });
 }
 
 catch (e) {
     console.error(e.message, 'Manual installation may be required.');
-    process.exit(2);
+    process.exit(1);
 }
