ed52c181852a414cd5938b752397327a14563a1f
Alexander Litty <me@alexlitty.com>
2017-05-06 23:54:17 -0700

    Moving tmp directory creation to separate file.

---
+++ scripts/utility/tmp.js
@@ -0,0 +1,25 @@
+const fs    = require('fs');
+const spawn = require('child_process').spawn;
+
+function dir(routine) {
+    const mktemp = spawn('mktemp', ['-d'], { stdio: 'pipe' });
+
+    let tmpDir = '';
+    mktemp.stdout.on('data', (data) => {
+        tmpDir += data.toString().trim();
+    });
+
+    mktemp.on('error', () => {});
+
+    mktemp.on('close', (code) => {
+        if (code !== 0) {
+            routine(new Error('Failed to create temporary directory'));
+        } else {
+            routine(null, tmpDir, () => {
+                fs.rmdir(tmpDir, () => {});
+            });
+        }
+    });
+}
+
+module.exports = { dir: dir }
