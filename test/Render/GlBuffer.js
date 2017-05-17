974a8098d75c32dd71aa73d7efaeac83eff85884
Alexander Litty <me@alexlitty.com>
2017-05-17 00:18:51 -0700

    Dynamically testing all GlBuffer typedefs.

--- test/Render/GlBuffer.js
+++ test/Render/GlBuffer.js
@@ -1,89 +1,92 @@
+const bufferTypes = {
+    FloatArrayBuffer: [0.65, 0.78, 42.09, 654],
+    IndexBuffer: [1, 8, 16, 32]
+}
+
 describe('GlBuffer', function() {
-    describe.only('FloatArrayBuffer', function() {
-        let ctx
+    let ctx
+
+    beforeEach(function() {
+        ctx = new randar.GraphicsContext()
+    })
+
+    for (bufferName in bufferTypes) {
+        let creatorName = bufferName[0].toLowerCase() + bufferName.slice(1)
+        let buffer
 
         beforeEach(function() {
-            ctx = new randar.GraphicsContext()
+            buffer = ctx[creatorName]()
         })
 
-        it('constructs empty, uninitialized, and unsynced', function() {
-            let buffer = ctx.floatArrayBuffer()
+        describe(bufferName, function() {
+            it('constructs empty, uninitialized, and unsynced', function() {
+                assert(!buffer.isInitialized())
+                assert(!buffer.isSynced())
 
-            assert(!buffer.isInitialized())
-            assert(!buffer.isSynced())
+                assert.equal(buffer.count(), 0)
+                assert(!buffer.isInitialized())
+                assert(!buffer.isSynced())
+            })
 
-            assert.equal(buffer.count(), 0)
-            assert(!buffer.isInitialized())
-            assert(!buffer.isSynced())
+            it('is not automatically initialized after appending', function() {
+                buffer.append(0.65)
+
+                assert.equal(buffer.count(), 1)
+                assert(!buffer.isInitialized())
+                assert(!buffer.isSynced())
+
+                buffer.append(0.78)
+                buffer.append(0.01)
+                buffer.append(1.00)
+
+                assert.equal(buffer.count(), 4)
+                assert(!buffer.isInitialized())
+                assert(!buffer.isSynced())
+            })
+
+            it('initializes with no data', function() {
+                buffer.initialize()
+                assert.equal(buffer.count(), 0)
+                assert(buffer.isInitialized())
+                assert(buffer.isSynced())
+            })
+
+            it('initializes with data', function() {
+                buffer.append(0.09)
+                assert.equal(buffer.count(), 1)
+                assert(!buffer.isInitialized())
+                assert(!buffer.isSynced())
+
+                buffer.initialize()
+                assert.equal(buffer.count(), 1)
+                assert(buffer.isInitialized())
+                assert(buffer.isSynced())
+            })
+
+            it('syncs with no data', function() {
+                buffer.sync()
+                assert.equal(buffer.count(), 0)
+                assert(buffer.isInitialized())
+                assert(buffer.isSynced())
+            })
+
+            it('syncs with data', function() {
+                buffer.append(0.62)
+                buffer.sync()
+
+                assert.equal(buffer.count(), 1)
+                assert(buffer.isInitialized())
+                assert(buffer.isSynced())
+
+                buffer.append(0.42)
+                buffer.append(4.2)
+                buffer.append(42.0)
+                buffer.sync()
+
+                assert.equal(buffer.count(), 4)
+                assert(buffer.isInitialized())
+                assert(buffer.isSynced())
+            });
         })
-
-        it('is not automatically initialized after appending', function() {
-            let buffer = ctx.floatArrayBuffer()
-            buffer.append(0.65)
-
-            assert.equal(buffer.count(), 1)
-            assert(!buffer.isInitialized())
-            assert(!buffer.isSynced())
-
-            buffer.append(0.78)
-            buffer.append(0.01)
-            buffer.append(1.00)
-
-            assert.equal(buffer.count(), 4)
-            assert(!buffer.isInitialized())
-            assert(!buffer.isSynced())
-        })
-
-        it('initializes with no data', function() {
-            let buffer = ctx.floatArrayBuffer()
-
-            buffer.initialize()
-            assert.equal(buffer.count(), 0)
-            assert(buffer.isInitialized())
-            assert(buffer.isSynced())
-        })
-
-        it('initializes with data', function() {
-            let buffer = ctx.floatArrayBuffer()
-
-            buffer.append(0.09)
-            assert.equal(buffer.count(), 1)
-            assert(!buffer.isInitialized())
-            assert(!buffer.isSynced())
-
-            buffer.initialize()
-            assert.equal(buffer.count(), 1)
-            assert(buffer.isInitialized())
-            assert(buffer.isSynced())
-        })
-
-        it('syncs with no data', function() {
-            let buffer = ctx.floatArrayBuffer()
-
-            buffer.sync()
-            assert.equal(buffer.count(), 0)
-            assert(buffer.isInitialized())
-            assert(buffer.isSynced())
-        })
-
-        it('syncs with data', function() {
-            let buffer = ctx.floatArrayBuffer()
-
-            buffer.append(0.62)
-            buffer.sync()
-
-            assert.equal(buffer.count(), 1)
-            assert(buffer.isInitialized())
-            assert(buffer.isSynced())
-
-            buffer.append(0.42)
-            buffer.append(4.2)
-            buffer.append(42.0)
-            buffer.sync()
-
-            assert.equal(buffer.count(), 4)
-            assert(buffer.isInitialized())
-            assert(buffer.isSynced())
-        });
-    })
+    }
 })
