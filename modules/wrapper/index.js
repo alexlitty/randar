6c591e19bd058095da8d08bf8512ce681330f1bc
Alexander Litty <me@alexlitty.com>
2017-05-17 21:50:27 -0700

    Removing wrapper-level vertex helper. Too complex.

--- modules/wrapper/index.js
+++ modules/wrapper/index.js
@@ -1,6 +1,5 @@
 const randar = require('../adapter');
 
 require('./color-ext')(randar);
-require('./vertex')(randar);
 
 module.exports = randar;
--- modules/wrapper/vertex.js
+++
@@ -1,5 +0,0 @@
-module.exports = (randar) => {
-    randar.vertex = function() {
-        return new (Function.prototype.bind.apply(randar.Vertex, arguments));
-    }
-};
