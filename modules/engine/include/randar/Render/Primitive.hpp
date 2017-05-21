17807bd09690e5b47108a5ff711df97b8675dd77
Alexander Litty <me@alexlitty.com>
2017-05-20 21:21:48 -0700

    Adding Primitive types.

---
+++ modules/engine/include/randar/Render/Primitive.hpp
@@ -0,0 +1,13 @@
+#ifndef RANDAR_RENDER_PRIMITIVE_HPP
+#define RANDAR_RENDER_PRIMITIVE_HPP
+
+namespace randar
+{
+    enum class Primitive
+    {
+        Point,
+        Triangle
+    };
+}
+
+#endif
