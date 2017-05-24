cbc41b643b1296ddf735ff9ae72342aa96628790
Alexander Litty <me@alexlitty.com>
2017-05-24 01:52:36 -0700

    Adding simple model test.

---
+++ test/Render/Model.js
@@ -0,0 +1,34 @@
+describe('Model', function() {
+    let ctx;
+    let win;
+    let fb;
+
+    before(function() {
+        ctx = new randar.GraphicsContext();
+        win = ctx.window(400, 400);
+        fb  = win.framebuffer();
+
+        fb.camera.position(randar.vector(0, 0, 1));
+        fb.camera.target(randar.vector(0, 0, 0));
+    });
+
+    after(function() {
+        win.close();
+    });
+
+    it('draws with transformations', function() {
+        this.timeout(10000);
+
+        let model = randar.model();
+        let geo   = randar.sphere(1);
+        model.geometry(geo);
+
+        for (let i = 0; i < 120; i++) {
+            model.move(randar.vector(0.02, 0.01, -0.05));
+
+            fb.clear(randar.color(0.3, 0.3, 0));
+            fb.draw(model);
+            win.present();
+        }
+    });
+});
