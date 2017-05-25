50b4301d6f5d1d6b88db1c46a2a47bbc739b0fb7
Alexander Litty <me@alexlitty.com>
2017-05-25 01:10:29 -0700

    Cleaning up timer, adding dedicated wait method.

--- modules/engine/include/randar/Utility/Timer.hpp
+++ modules/engine/include/randar/Utility/Timer.hpp
@@ -11,11 +11,37 @@ namespace randar
         std::chrono::high_resolution_clock::time_point start;
 
     public:
+        /**
+         * Constructor.
+         *
+         * Starts the timer immediately, setting the start time to the current
+         * time.
+         */
         Timer();
 
-        double getMilliseconds() const;
-        double getMicroseconds() const;
-        std::string get() const;
+        /**
+         * Resets the timer.
+         *
+         * Sets the start time to the current time.
+         */
+        void reset();
+
+        /**
+         * Returns the elapsed time.
+         */
+        double microseconds() const;
+        double milliseconds() const;
+        double seconds() const;
+
+        /**
+         * Waits until a period of microseconds have elapsed.
+         */
+        void wait(uint32_t microseconds) const;
+
+        /**
+         * Generates a human-friendly representation of elapsed time.
+         */
+        std::string toString() const;
     };
 }
 
--- modules/engine/src/Utility/Timer.cpp
+++ modules/engine/src/Utility/Timer.cpp
@@ -1,30 +1,59 @@
 #include <randar/Utility/Timer.hpp>
+#include <randar/Utility/Wait.hpp>
 
+// Constructor.
 randar::Timer::Timer()
+{
+    this->reset();
+}
+
+// Resets the timer.
+void randar::Timer::reset()
 {
     this->start = std::chrono::high_resolution_clock::now();
 }
 
-double randar::Timer::getMilliseconds() const
-{
-    return std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::high_resolution_clock::now() - this->start
-    ).count();
-}
-
-double randar::Timer::getMicroseconds() const
+// Returns the elapsed time in a desired time unit.
+double randar::Timer::microseconds() const
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(
         std::chrono::high_resolution_clock::now() - this->start
     ).count();
 }
 
-std::string randar::Timer::get() const
+double randar::Timer::milliseconds() const
 {
-    int32_t micro = this->getMicroseconds();
+    return this->microseconds() * 1000;
+}
+
+double randar::Timer::seconds() const
+{
+    return this->microseconds() * 1000000;
+}
+
+// Waits until a period of microseconds have elapsed.
+void randar::Timer::wait(uint32_t microseconds) const
+{
+    uint32_t elapsed = static_cast<uint32_t>(this->microseconds());
+    if (elapsed >= microseconds) {
+        return;
+    }
+
+    randar::wait(microseconds - elapsed);
+}
+
+// Generates a human-friendly representation of elapsed time.
+std::string randar::Timer::toString() const
+{
+    int32_t micro = this->microseconds();
     if (micro <= 1000) {
         return std::to_string(micro) + "us";
     }
 
-    return std::to_string((int32_t)this->getMilliseconds()) + "ms";
+    int32_t milli = this->milliseconds();
+    if (milli <= 1000) {
+        return std::to_string(micro) + "ms";
+    }
+
+    return std::to_string(static_cast<int32_t>(this->seconds())) + "s";
 }
