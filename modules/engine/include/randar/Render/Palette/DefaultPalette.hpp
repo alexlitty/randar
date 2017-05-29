6795b9d375abe213502379ed3f2b299cca316324
Alexander Litty <me@alexlitty.com>
2017-05-28 22:59:56 -0700

    Adding a global default palette.

---
+++ modules/engine/include/randar/Render/Palette/DefaultPalette.hpp
@@ -0,0 +1,11 @@
+#ifndef RANDAR_RENDER_DEFAULT_PALETTE_HPP
+#define RANDAR_RENDER_DEFAULT_PALETTE_HPP
+
+#include <randar/Render/Palette/VaryingPalette.hpp>
+
+namespace randar
+{
+    extern VaryingPalette defaultPalette;
+}
+
+#endif
---
+++ modules/engine/src/Render/Palette/DefaultPalette.cpp
@@ -0,0 +1,6 @@
+#include <randar/Render/Palette/DefaultPalette.hpp>
+
+randar::VaryingPalette randar::defaultPalette(
+    randar::Color(0.40, 0.40, 0.20),
+    randar::Color(0.05, 0.05, 0.05)
+);
