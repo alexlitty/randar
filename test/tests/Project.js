8d05b585f04c65979e2e046e61cff89d70556bb7
Alexander Litty <me@alexlitty.com>
2017-05-30 03:03:27 -0700

    Expanding project tests.

--- test/tests/Project.js
+++ test/tests/Project.js
@@ -1,4 +1,4 @@
-describe('Project', function() {
+describe.only('Project', function() {
     let tmpDir;
 
     before(function() {
@@ -16,6 +16,19 @@ describe('Project', function() {
         project.directory(tmpDir);
         assert.equal(project._onDisk, false);
 
+
+        let geo = randar.geometry();
+        for (let i = 0; i < 3; i++) {
+            geo.append(randar.vertex(0.3, 0.2, 0.1));
+        }
+
+        let bin = project.addBin('Primary bin');
+        assert.equal(Object.keys(project.bins).length, 1);
+        assert.equal(Object.keys(project.binFolders).length, 1);
+
+        project.addBinItem(geo, 'Test Geometry', bin);
+        assert.equal(Object.keys(project.binItems).length, 1);
+
         project.save();
         assert.equal(project._onDisk, true);
     });
@@ -24,5 +37,15 @@ describe('Project', function() {
         let project = randar.project(tmpDir);
         assert.equal(project._onDisk, true);
         assert.equal(project.directory().toString(), tmpDir.toString());
+
+        assert.equal(Object.keys(project.bins).length, 1);
+        assert.equal(Object.keys(project.binFolders).length, 1);
+        assert.equal(Object.keys(project.binItems).length, 1);
+
+        project.loadBinItem(project.binItems[0]);
+
+        let geo = project.binItems[0].object;
+        assert.equal(geo.vertices.count(), 1);
+        assert.equal(geo.indices.count(), 3);
     });
 });
