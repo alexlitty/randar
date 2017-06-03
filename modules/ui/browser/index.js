7553e54ef2d0dc661912feaebeef0d53eb0334eb
Alexander Litty <me@alexlitty.com>
2017-06-03 00:39:03 -0700

    Reorganizing UI for a new idea.

--- modules/ui/browser/index.js
+++ modules/ui/browser/index.js
@@ -1,9 +1,22 @@
 const {app, BrowserWindow} = require('electron');
+
+const fs   = require('fs');
 const path = require('path');
-const url = require('url');
+const url  = require('url');
 
-var win;
+const args = process.argv;
+if (args.length < 3) {
+    console.error('No view provided');
+    process.exit(1);
+}
 
+const filepath = path.join(__dirname, 'build', `${args[2]}.html`);
+if (!fs.existsSync(filepath)) {
+    console.error('Nonexistent view');
+    process.exit(1);
+}
+
+let win;
 app.on('ready', () => {
     win = new BrowserWindow({
         minWidth  : 600,
@@ -26,7 +39,7 @@ app.on('ready', () => {
         win = null;
     });
 
-    win.loadURL('file://' + __dirname + '/build/ui.html');
+    win.loadURL(`file://${filepath}`);
 });
 
 app.on('windows-all-closed', () => {
---
+++ modules/ui/index.js
@@ -0,0 +1,1 @@
+const randar = require('../../wrapper');
