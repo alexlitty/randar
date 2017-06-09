a27770f84a9fc4ff3409b276913a55ee779604a0
Alexander Litty <me@alexlitty.com>
2017-06-08 21:09:53 -0700

    Adding context menu to bins dialog.

--- modules/ui/browser-client/js/components/project/bins.js
+++ modules/ui/browser-client/js/components/project/bins.js
@@ -1,6 +1,19 @@
 ui.component('project-bins', {
     data: function() {
         return {
+            menuItems: [
+                {
+                    text: 'Bins',
+                    contextMenu: [
+                        [
+                            { text: 'New bin', action: function() {
+                                ui.emit('view.open', { type: 'new-bin' });
+                            }}
+                        ]
+                    ]
+                }
+            ],
+
             bins: ui.project.bins
         }
     },
@@ -8,15 +21,18 @@ ui.component('project-bins', {
     methods: {
         onClose: function() {
             ui.exit();
+        },
+
+        createBin: function() {
         }
     },
 
     template: `
         <div class="dialog bins">
-            <menu-bar :onClose="onClose"></menu-bar>
+            <menu-bar :items="menuItems" :onClose="onClose"></menu-bar>
 
             <div v-if="bins && bins.length">
-                <project-folder v-for="(bin, binId) in bins" folderId="binId">
+                <project-folder v-for="(bin, binId) in bins" :folderId="binId">
                 </project-folder>
             </div>
 
