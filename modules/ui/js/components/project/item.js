e3d88eab74fa3efaa3e647bd33df6f1063b39191
Alexander Litty <me@alexlitty.com>
2017-06-17 19:24:09 -0700

    Monitoring geometry upon request.

--- modules/ui/js/components/project/item.js
+++ modules/ui/js/components/project/item.js
@@ -3,9 +3,15 @@ ui.component('project-item', {
         item: Object
     },
 
+    methods: {
+        onMonitor: function() {
+            ui.monitor(this.item);
+        }
+    },
+
     template: `
         <div class="item">
-            <div class="data" :class="item.kind">
+            <div class="data" :class="item.kind" @click="onMonitor">
                 {{ item.name }}
             </div>
         </div>
