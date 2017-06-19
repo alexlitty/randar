1db3abbac377839fd0060ab88d899f5b94ffd0b3
Alexander Litty <me@alexlitty.com>
2017-06-18 20:39:53 -0700

    Adding primitive and index count info.

--- modules/ui/js/components/dialogs/geometry.js
+++ modules/ui/js/components/dialogs/geometry.js
@@ -3,6 +3,12 @@ ui.component('dialog-geometry', {
         item: Object
     },
 
+    computed: {
+        geo: function() {
+            return this.item.object()
+        }
+    },
+
     methods: {
         onClose: function() {
             this.$emit('close');
@@ -12,6 +18,16 @@ ui.component('dialog-geometry', {
     template: `
         <div class="dialog draggable geometry">
             <menu-bar :text="item.name" :onClose="onClose"></menu-bar>
+
+            <div class="info">
+                <div v-if="geo.primitive === randar.Primitive_Point">
+                    {{ geo.indices.count() }} Points
+                </div>
+
+                <div v-if="geo.primitive === randar.Primitive_Triangle">
+                    {{ geo.indices.count() / 3 }} Triangles
+                </div>
+            </div>
         </div>
     `
 });
