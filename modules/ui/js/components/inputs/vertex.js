3b64ffa7e462880757a430d522ea34306188d826
Alexander Litty <me@alexlitty.com>
2017-06-21 23:32:47 -0700

    Adding color input to vertex input.

--- modules/ui/js/components/inputs/vertex.js
+++ modules/ui/js/components/inputs/vertex.js
@@ -7,18 +7,37 @@ ui.component('input-vertex', {
         onInputPosition: function(position) {
             this.value.position = position;
             this.$emit('input', this.value);
+        },
+
+        onInputColor: function(color) {
+            this.value.color = color;
+            this.$emit('input', this.value);
         }
     },
 
     template: `
         <div class="input vertex">
-            <div class="position">
-                Position
-                <input-vector3
-                 :value.sync="value.position"
-                 @input="onInputPosition">
-                </input-vector3>
-            </div>
+            <table>
+                <tr>
+                    <td>Position</td>
+                    <td>
+                        <input-vector3
+                         :value.sync="value.position"
+                         @input="onInputPosition">
+                        </input-vector3>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Color</td>
+                    <td>
+                        <input-color
+                         :value.sync="value.color"
+                         @input="onInputColor">
+                        </input-color>
+                    </td>
+                </tr>
+            </table>
         </div>
     `
 });
