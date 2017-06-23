98246b54d4320bbcb87d558d9fb2f593a994e680
Alexander Litty <me@alexlitty.com>
2017-06-22 23:42:40 -0700

    Improved input handling for toColor.

--- modules/wrapper/toColor.js
+++ modules/wrapper/toColor.js
@@ -1,3 +1,16 @@
+function toNumber(value) {
+    if (typeof value === 'number') {
+        return value;
+    }
+
+    let result = parseFloat(value);
+    if (isNaN(result)) {
+        throw new Error(`Invalid channel value: ${value}`);
+    }
+
+    return result;
+}
+
 module.exports = (randar) => {
     randar.toColor = function(obj) {
         if (!obj) {
@@ -7,11 +20,11 @@ module.exports = (randar) => {
         let color = randar.color();
         for (let channel of ['r', 'g', 'b', 'a']) {
             if (obj[channel]) {
-                color[channel](obj[channel]);
+                color[channel](toNumber(obj[channel]));
             }
 
             else if (obj[channel + 'Int']) {
-                color[channel + 'Int'](obj[channel + 'Int']);
+                color[channel + 'Int'](toNumber(obj[channel + 'Int']));
             }
 
             else if (channel === 'a') {
