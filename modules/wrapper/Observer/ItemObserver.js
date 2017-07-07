1c027b13d1dc74dbd7d8070b29408d8a30b1d21a
Alexander Litty <me@alexlitty.com>
2017-07-06 20:36:10 -0700

    Starting an Item Observer, to replace monitors.

---
+++ modules/wrapper/Observer/ItemObserver.js
@@ -0,0 +1,63 @@
+module.exports = (randar) => {
+    /**
+     * A human-friendly project item observer.
+     */
+    randar.ItemObserver = function(ctx, item) {
+        this.win = ctx.window(800, 600);
+        this.item(item);
+    }
+
+    /**
+     * Gets or sets the item to observe.
+     */
+    randar.ItemObserver.prototype.item = function(item) {
+        // Get the item.
+        if (!item) {
+            return this._item;
+        }
+
+        // Set the item.
+        else {
+            this._item = item;
+        }
+    }
+
+    /**
+     * Runs a frame on the observer.
+     */
+    randar.ItemObserver.prototype.run = function() {
+        this.win.clear();
+        this.draw();
+        this.win.present();
+    }
+
+    /**
+     * Draws the item to the window.
+     */
+    randar.ItemObserver.prototype.draw = function() {
+        let item = this.item();
+        if (!item) {
+            return;
+        }
+
+        let object = item.object();
+        if (!object) {
+            throw new Error('Item has no object');
+        }
+
+        if (!object.kind || !object.kind()) {
+            throw new Error('Item object has no kind');
+        }
+
+        if (object.kind() === 'geometry') {
+            this.win.draw(object);
+        }
+
+        else if (object.kind() === 'image') {
+        }
+
+        else {
+            throw new Error('Observing item kind not implemented');
+        }
+    }
+}
--- modules/wrapper/index.js
+++ modules/wrapper/index.js
@@ -4,5 +4,6 @@ require('./toColor')(randar);
 require('./toVector')(randar);
 require('./toVertex')(randar);
 require('./Project/Project')(randar);
+require('./Observer/ItemObserver')(randar);
 
 module.exports = randar;
