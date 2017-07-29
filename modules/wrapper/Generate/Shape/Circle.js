acd8c8cb52262b38380f2495dfe7161647708cbd
Alexander Litty <me@alexlitty.com>
2017-07-29 12:10:42 -0700

    Adding circle generation.

---
+++ modules/wrapper/Generate/Shape/Circle.js
@@ -0,0 +1,28 @@
+module.exports = (randar) => {
+    randar.generate.circle = function(geometry, radius, points) {
+        if (points < 3) {
+            throw new Error('Circles can only be generated with 3 or more points');
+        }
+
+        let quat = randar.quaternion(
+            randar.vector(0, 0, 1),
+            randar.angle((1 / points) * randar.TWO_PI)
+        );
+
+        let center = randar.vector(0, 0, 0);
+        let outter = randar.vector(radius, 0, 0);
+
+        let vertex = randar.vertex();
+        for (let i = 0; i < points; i++) {
+            vertex.position = center;
+            geometry.append(vertex);
+
+            vertex.position = outter;
+            geometry.append(vertex);
+
+            outter = quat.transform(outter);
+            vertex.position = outter;
+            geometry.append(vertex);
+        }
+    }
+};
--- modules/wrapper/index.js
+++ modules/wrapper/index.js
@@ -3,8 +3,12 @@ const randar = require('../adapter');
 require('./toColor')(randar);
 require('./toVector')(randar);
 require('./toVertex')(randar);
+
 require('./Scene/Scene')(randar);
 require('./Project/Project')(randar);
 require('./Observer/ItemObserver')(randar);
+
+randar.generate = { };
+require('./Generate/Shape/Circle')(randar);
 
 module.exports = randar;
