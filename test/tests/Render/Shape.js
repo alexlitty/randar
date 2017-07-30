91f3c8fc957d5f2b34a7ad9b49ddbba901c69afa
Alexander Litty <me@alexlitty.com>
2017-07-29 23:57:14 -0700

    Adding new additional shapes to tests.

--- test/tests/Render/Shape.js
+++ test/tests/Render/Shape.js
@@ -32,11 +32,41 @@ describe('Shape', function() {
         win.close();
     });
 
-    it('generates spheres', function() {
+    it('generates circles', function() {
         this.timeout(10000);
 
-        for (let i = 0; i < 60; i++) {
-            let geo = randar.generate.sphere(i, 24, 24);
+        for (let k = 3; k < 30; k += 5) {
+            for (let i = 0; i < 60; i++) {
+                let geo = randar.generate.circle(i, k);
+
+                moveCamera();
+                win.clear();
+                win.draw(geo);
+                win.present();
+            }
+        }
+    });
+
+    it('generates cones', function() {
+        this.timeout(10000);
+
+        for (let k = 3; k < 30; k+= 5) {
+            for (let i = 0; i < 60; i++) {
+                let geo = randar.generate.cone(i, 90, k);
+
+                moveCamera();
+                win.clear();
+                win.draw(geo);
+                win.present();
+            }
+        }
+    });
+
+    it('generates cubes', function() {
+        this.timeout(10000);
+
+        for (let i = 0; i < 180; i++) {
+            let geo = randar.generate.cube(i);
 
             moveCamera();
             win.clear();
@@ -50,6 +80,60 @@ describe('Shape', function() {
 
         for (let i = 0; i < 180; i++) {
             let geo = randar.generate.cuboid(i, i * 0.75, i * 0.5);
+
+            moveCamera();
+            win.clear();
+            win.draw(geo);
+            win.present();
+        }
+    });
+
+    it('generates cylinders', function() {
+        this.timeout(10000);
+
+        for (let k = 3; k <= 48; k *= 2) {
+            for (let i = 0; i < 90; i++) {
+                let geo = randar.generate.cylinder(i, 60, k);
+
+                moveCamera();
+                win.clear();
+                win.draw(geo);
+                win.present();
+            }
+        }
+    });
+
+    it('generates rectangles', function() {
+        this.timeout(10000);
+
+        for (let i = 0; i < 90; i++) {
+            let geo = randar.generate.rectangle(i, i * 0.75);
+
+            moveCamera();
+            win.clear();
+            win.draw(geo);
+            win.present();
+        }
+    });
+
+    it('generates spheres', function() {
+        this.timeout(10000);
+
+        for (let i = 0; i < 60; i++) {
+            let geo = randar.generate.sphere(i, 24, 24);
+
+            moveCamera();
+            win.clear();
+            win.draw(geo);
+            win.present();
+        }
+    });
+
+    it('generates squares', function() {
+        this.timeout(10000);
+
+        for (let i = 0; i < 90; i++) {
+            let geo = randar.generate.square(i);
 
             moveCamera();
             win.clear();
