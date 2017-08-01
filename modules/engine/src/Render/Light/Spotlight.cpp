3da516da34e9cfb1c65ae35c9c10e1a3af923315
Alexander Litty <me@alexlitty.com>
2017-08-01 00:23:46 -0700

    Adding test position to spotlights.

--- modules/engine/src/Render/Light/Spotlight.cpp
+++ modules/engine/src/Render/Light/Spotlight.cpp
@@ -4,7 +4,10 @@
 randar::Spotlight::Spotlight(randar::GraphicsContext& context)
 : randar::BaseLight(context)
 {
-
+    this->framebuffer.camera().ortho(-10, 10, -10, 10);
+    this->framebuffer.camera().range(-10, 20);
+    this->framebuffer.camera().position(Vector3(0, 0, 5));
+    this->framebuffer.camera().target(Vector3(0, 0, 0));
 }
 
 // Generates the lightmap of a world.
