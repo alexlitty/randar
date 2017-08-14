8cf2e3891711b92d446c82bc8a5f23e34918b7de
Alexander Litty <me@alexlitty.com>
2017-08-13 21:46:43 -0700

    Introducing skeleton concept.

---
+++ modules/engine/include/randar/Render/Skeleton.hpp
@@ -0,0 +1,110 @@
+#ifndef RANDAR_RENDER_SKELETON_HPP
+#define RANDAR_RENDER_SKELETON_HPP
+
+#include <map>
+#include <vector>
+#include <randar/Render/Joint.hpp>
+
+namespace randar
+{
+    /**
+     * Skeleton for models to represent joint states.
+     */
+    class Skeleton
+    {
+        /**
+         * Joints in the skeleton.
+         */
+        std::vector<Joint> joints;
+
+        /**
+         * Names of joints in the skeleton.
+         */
+        std::vector<std::string> jointNames;
+
+        /**
+         * Map of joint parents.
+         */
+        std::map<uint32_t, uint32_t> jointParents;
+
+    public:
+        /**
+         * Default constructor.
+         */
+        Skeleton();
+
+        /**
+         * Assignment.
+         */
+        Skeleton(const Skeleton& other);
+        Skeleton& operator =(const Skeleton& other);
+        Skeleton(Skeleton&& other);
+
+        /**
+         * Destructor.
+         */
+        ~Skeleton();
+
+        /**
+         * Clears the joints on the skeleton.
+         */
+        void clear();
+
+        /**
+         * Adds a parentless joint to the skeleton, returning the added joint.
+         *
+         * Throws an error if a joint with the same name already exists.
+         */
+        Joint& add(const std::string& name);
+
+        /**
+         * Adds a child joint to the skeleton, returning the added joint.
+         *
+         * Throws an error if a joint with the same name already exists, or a
+         * joint with the desired parent name does not exist.
+         */
+        Joint& add(const std::string& name, const std::string& parentName);
+
+        /**
+         * Counts the joints on the skeleton.
+         */
+        uint32_t jointCount() const;
+
+        /**
+         * Retrieves a joint by name or index.
+         *
+         * Throws an error if the joint does not exist.
+         */
+        Joint& joint(const std::string& name);
+        Joint& joint(uint32_t index);
+
+        /**
+         * Retrieves the name of a joint by its index.
+         *
+         * Throws an error if the joint does not exist.
+         */
+        std::string jointName(uint32_t index) const;
+
+        /**
+         * Retrieves the index of a joint by its name.
+         *
+         * Throws an error if the joint does not exist.
+         */
+        uint32_t jointIndex(const std::string& name) const;
+
+        /**
+         * Calculates the matrix of a joint and its parents.
+         *
+         * Throws an error if the joint does not exist.
+         */
+        glm::mat4 matrix(const std::string& name);
+        glm::mat4 matrix(uint32_t index);
+    };
+
+    /**
+     * Node.js helper.
+     */
+    Skeleton skeleton();
+}
+
+#endif
---
+++ modules/engine/src/Render/Skeleton.cpp
@@ -0,0 +1,126 @@
+#include <randar/Render/Skeleton.hpp>
+
+// Default constructor.
+randar::Skeleton::Skeleton()
+{
+
+}
+
+// Assignment.
+randar::Skeleton::Skeleton(const randar::Skeleton& other)
+{
+    *this = other;
+}
+
+randar::Skeleton& randar::Skeleton::operator =(const randar::Skeleton& other)
+{
+    this->clear();
+
+    this->joints = other.joints;
+    this->jointNames = other.jointNames;
+    this->jointParents = other.jointParents;
+
+    return *this;
+}
+
+randar::Skeleton::Skeleton(randar::Skeleton&& other)
+{
+    *this = other;
+    other.clear();
+}
+
+// Destructor.
+randar::Skeleton::~Skeleton()
+{
+    this->clear();
+}
+
+// Clears the joints on the skeleton.
+void randar::Skeleton::clear()
+{
+    this->joints.clear();
+    this->jointNames.clear();
+    this->jointParents.clear();
+}
+
+// Adds a parentless joint to the skeleton.
+randar::Joint& randar::Skeleton::add(const std::string& name)
+{
+    this->joints.emplace_back();
+    this->jointNames.push_back(name);
+    return this->joints[this->joints.size() - 1];
+}
+
+// Adds a child joint to the skeleton.
+randar::Joint& randar::Skeleton::add(const std::string& name, const std::string& parentName)
+{
+    randar::Joint& joint = this->add(name);
+    this->jointParents[this->joints.size() - 1] = this->jointIndex(parentName);
+    return joint;
+}
+
+// Counts the joints on the skeleton.
+uint32_t randar::Skeleton::jointCount() const
+{
+    return this->joints.size();
+}
+
+// Retrieves a joint by its name or index.
+randar::Joint& randar::Skeleton::joint(const std::string& name)
+{
+    return this->joint(this->jointIndex(name));
+}
+
+randar::Joint& randar::Skeleton::joint(uint32_t index)
+{
+    if (this->jointNames.size() >= index) {
+        throw new std::runtime_error("Joint does not exist");
+    }
+
+    return this->joints[index];
+}
+
+// Retrieves the name of a joint by its index.
+std::string randar::Skeleton::jointName(uint32_t index) const
+{
+    if (this->jointNames.size() >= index) {
+        throw new std::runtime_error("Joint does not exist");
+    }
+
+    return this->jointNames[index];
+}
+
+// Retrieves the index of a joint by its name.
+uint32_t randar::Skeleton::jointIndex(const std::string& name) const
+{
+    for (uint32_t i = 0; i < this->jointNames.size(); i++) {
+        if (this->jointNames[i] == name) {
+            return i;
+        }
+    }
+
+    throw new std::runtime_error("Joint does not exist");
+}
+
+// Calculates the matrix of a joint and its parents.
+glm::mat4 randar::Skeleton::matrix(const std::string& name)
+{
+    return this->matrix(this->jointIndex(name));
+}
+
+glm::mat4 randar::Skeleton::matrix(uint32_t index)
+{
+    randar::Joint& joint = this->joint(index);
+
+    if (!this->jointParents.count(index)) {
+        return joint.transformMatrix();
+    }
+
+    return joint.transformMatrix() * this->matrix(this->jointParents[index]);
+}
+
+// Node.js helper.
+randar::Skeleton randar::skeleton()
+{
+    return randar::Skeleton();
+}
