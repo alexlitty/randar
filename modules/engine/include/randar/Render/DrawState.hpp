e1bac799927fc6497ac3fb76048ac5849981deda
Alexander Litty <me@alexlitty.com>
2017-08-13 22:45:38 -0700

    Adding draw state object.

---
+++ modules/engine/include/randar/Render/DrawState.hpp
@@ -0,0 +1,28 @@
+#ifndef RANDAR_RENDER_DRAW_STATE_HPP
+#define RANDAR_RENDER_DRAW_STATE_HPP
+
+#include <randar/Render/Model.hpp>
+#include <randar/Render/TextureCollection.hpp>
+
+namespace randar
+{
+    class BaseLight;
+    typedef std::vector<BaseLight*> LightCollection;
+
+    /**
+     * State object for fine geometry drawing control.
+     *
+     * Passed into canvas drawing methods. Null properties are substituted with
+     * defaults.
+     */
+    struct DrawState
+    {
+        Transform* transform = nullptr;
+        Skeleton* skeleton = nullptr;
+        TextureCollection* textures = nullptr;
+        LightCollection* lights = nullptr;
+        ShaderProgram* program = nullptr;
+    };
+}
+
+#endif
