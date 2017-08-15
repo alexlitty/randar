3ad60ea811f06bfd5cee9a4604854376045d3f2f
Alexander Litty <me@alexlitty.com>
2017-08-14 22:34:16 -0700

    Transforming model position by joint matrices.

--- modules/engine/src/Render/Shader.cpp
+++ modules/engine/src/Render/Shader.cpp
@@ -182,6 +182,22 @@ std::string randar::Shader::defaultCode(randar::ShaderType type)
                     // Model space position.
                     vec4 modelPosition = vec4(vec3(modelMatrix * vec4(fragmentPosition, 1.0)), 1.0);
 
+                    if (jointWeights.x > 0) {
+                        modelPosition = (jointMatrices[jointIndices.x] * modelPosition) * (jointWeights.x / 255);
+                    }
+
+                    if (jointWeights.y > 0) {
+                        modelPosition = (jointMatrices[jointIndices.y] * modelPosition) * (jointWeights.y / 255);
+                    }
+
+                    if (jointWeights.z > 0) {
+                        modelPosition = (jointMatrices[jointIndices.z] * modelPosition) * (jointWeights.z / 255);
+                    }
+
+                    if (jointWeights.w > 0) {
+                        modelPosition = (jointMatrices[jointIndices.w] * modelPosition) * (jointWeights.w / 255);
+                    }
+
                     // Camera space position.
                     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
