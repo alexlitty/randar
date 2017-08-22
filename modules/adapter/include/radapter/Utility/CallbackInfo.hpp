b107959eaae51574b8a92fe7189a815305d38548
Alexander Litty <github@alexlitty.com>
2017-08-21 17:14:48 -0700

    Adding comments to CallbackInfo.

--- modules/adapter/include/radapter/Utility/CallbackInfo.hpp
+++ modules/adapter/include/radapter/Utility/CallbackInfo.hpp
@@ -15,10 +15,26 @@ namespace radapter
      */
     struct CallbackInfo
     {
+        /**
+         * N-API environment hint.
+         *
+         * This value cannot be cached around, so neither should this struct.
+         */
         napi_env env;
+
+        /**
+         * "this" argument in Node.js.
+         */
         napi_value self;
+
+        /**
+         * Arguments passed to the callback.
+         */
         std::vector<napi_value> args;
 
+        /**
+         * Constructor.
+         */
         CallbackInfo(napi_env env, napi_callback_info info);
     };
 }
