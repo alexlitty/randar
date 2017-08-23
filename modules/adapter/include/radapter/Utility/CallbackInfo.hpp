91f50307e78dfbf38e7b2b4df691dc57addcb977
Alexander Litty <me@alexlitty.com>
2017-08-22 22:14:41 -0700

    Adding unwrap helper in callback info.

--- modules/adapter/include/radapter/Utility/CallbackInfo.hpp
+++ modules/adapter/include/radapter/Utility/CallbackInfo.hpp
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <radapter/Utility/CheckNapi.hpp>
+#include <radapter/Utility/NapiValue.hpp>
 
 namespace radapter
 {
@@ -36,6 +37,21 @@ namespace radapter
          * Constructor.
          */
         CallbackInfo(napi_env env, napi_callback_info info);
+
+        /**
+         * Unwraps "this" into the native object being wrapped.
+         */
+        template <class T>
+        T& unwrap()
+        {
+            void* result;
+            checkNapi("unwrapping Randar object", this->env, napi_unwrap(
+                this->env,
+                this->self,
+                &result
+            ));
+            return *reinterpret_cast<T*>(result);
+        }
     };
 }
 
