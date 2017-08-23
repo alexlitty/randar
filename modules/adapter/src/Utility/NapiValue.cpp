7af592bcdba2ce6018c7be9651185d5f824da186
Alexander Litty <me@alexlitty.com>
2017-08-22 23:10:19 -0700

    Adding more napiValue specializations.

--- modules/adapter/src/Utility/NapiValue.cpp
+++ modules/adapter/src/Utility/NapiValue.cpp
@@ -3,6 +3,12 @@
 
 namespace radapter
 {
+    // Floating point specializations.
+    template <> napi_value napiValue(napi_env env, float value)
+    {
+        return radapter::napiValue<double>(env, value);
+    }
+
     template <> napi_value napiValue(napi_env env, double value)
     {
         napi_value result;
@@ -14,8 +20,75 @@ namespace radapter
         return result;
     }
 
-    template <> napi_value napiValue(napi_env env, float value)
+    // Integer specializations.
+    template <> napi_value napiValue(napi_env env, int8_t value)
     {
-        return radapter::napiValue<double>(env, value);
+        return radapter::napiValue<int32_t>(env, value);
+    }
+
+    template <> napi_value napiValue(napi_env env, int16_t value)
+    {
+        return radapter::napiValue<int32_t>(env, value);
+    }
+
+    template <> napi_value napiValue(napi_env env, int32_t value)
+    {
+        napi_value result;
+        radapter::checkNapi("create napi int32", env, napi_create_int32(
+            env,
+            value,
+            &result
+        ));
+        return result;
+    }
+
+    template <> napi_value napiValue(napi_env env, int64_t value)
+    {
+        napi_value result;
+        radapter::checkNapi("create napi int64", env, napi_create_int64(
+            env,
+            value,
+            &result
+        ));
+        return result;
+    }
+
+    template <> napi_value napiValue(napi_env env, uint8_t value)
+    {
+        return radapter::napiValue<uint32_t>(env, value);
+    }
+
+    template <> napi_value napiValue(napi_env env, uint16_t value)
+    {
+        return radapter::napiValue<uint32_t>(env, value);
+    }
+
+    template <> napi_value napiValue(napi_env env, uint32_t value)
+    {
+        napi_value result;
+        radapter::checkNapi("create napi uint32", env, napi_create_uint32(
+            env,
+            value,
+            &result
+        ));
+        return result;
+    }
+
+    // String specialization.
+    template <> napi_value napiValue(napi_env env, const char* value)
+    {
+        return radapter::napiValue(env, std::string(value));
+    }
+
+    template <> napi_value napiValue(napi_env env, std::string value)
+    {
+        napi_value result;
+        radapter::checkNapi("create napi utf8 string", env, napi_create_string_utf8(
+            env,
+            value.c_str(),
+            -1,
+            &result
+        ));
+        return result;
     }
 }
