b56b00b7e8b3d816a1460781e61f87008d79f053
Alexander Litty <me@alexlitty.com>
2017-08-28 19:52:04 -0700

    Adding more Color adapter methods.

--- modules/adapter/include/radapter/Render/Color.hpp
+++ modules/adapter/include/radapter/Render/Color.hpp
@@ -9,11 +9,28 @@ namespace radapter
     struct Color
     {
         static std::string className;
+
+        /**
+         * Meta methods.
+         */
         static std::vector<napi_property_descriptor> properties();
         static randar::Color* instance(radapter::CallbackInfo& info);
 
-        static napi_value rGet(CallbackInfo& info);
-        static napi_value rSet(CallbackInfo& info);
+        /**
+         * Channel getters and setters via floats.
+         */
+        static napi_value r(CallbackInfo& info);
+        static napi_value g(CallbackInfo& info);
+        static napi_value b(CallbackInfo& info);
+        static napi_value a(CallbackInfo& info);
+
+        /**
+         * Channel getters and setters via integers.
+         */
+        static napi_value rInt(CallbackInfo& info);
+        static napi_value gInt(CallbackInfo& info);
+        static napi_value bInt(CallbackInfo& info);
+        static napi_value aInt(CallbackInfo& info);
     };
 }
 
--- modules/adapter/src/Render/Color.cpp
+++ modules/adapter/src/Render/Color.cpp
@@ -2,11 +2,16 @@
 
 std::string radapter::Color::className = "Color";
 
+// Meta methods.
 std::vector<napi_property_descriptor> radapter::Color::properties()
 {
     return {
-        radapter::getter<radapter::Color::rGet>("r"),
-        radapter::setter<radapter::Color::rSet>("r")
+        radapter::methodProperty<radapter::Color::r>("r"),
+        radapter::methodProperty<radapter::Color::g>("g"),
+        radapter::methodProperty<radapter::Color::b>("b"),
+        radapter::methodProperty<radapter::Color::a>("a"),
+
+        radapter::methodProperty<radapter::Color::rInt>("rInt")
     };
 }
 
@@ -15,16 +20,100 @@ randar::Color* radapter::Color::instance(radapter::CallbackInfo& info)
     return new randar::Color();
 }
 
-napi_value radapter::Color::rGet(radapter::CallbackInfo& info)
+// Channel getters and setters.
+napi_value radapter::Color::r(radapter::CallbackInfo& info)
 {
-    return napiValue(info.env, info.unwrap<randar::Color>().r());
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.r(radapter::nativeValue<float>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.r());
 }
 
-napi_value radapter::Color::rSet(radapter::CallbackInfo& info)
+napi_value radapter::Color::g(radapter::CallbackInfo& info)
 {
-    info.unwrap<randar::Color>().r(
-        radapter::nativeValue<float>(info.env, info.args[0])
-    );
+    auto &color = info.unwrap<randar::Color>();
 
-    return info.args[0];
+    if (info.args.size()) {
+        color.g(radapter::nativeValue<float>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.g());
+}
+
+napi_value radapter::Color::b(radapter::CallbackInfo& info)
+{
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.b(radapter::nativeValue<float>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.b());
+}
+
+napi_value radapter::Color::a(radapter::CallbackInfo& info)
+{
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.a(radapter::nativeValue<float>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.a());
+}
+
+// Channel getters and setters via integers.
+napi_value radapter::Color::rInt(radapter::CallbackInfo& info)
+{
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.rInt(radapter::nativeValue<uint8_t>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.rInt());
+}
+
+napi_value radapter::Color::gInt(radapter::CallbackInfo& info)
+{
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.gInt(radapter::nativeValue<uint8_t>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.gInt());
+}
+
+napi_value radapter::Color::bInt(radapter::CallbackInfo& info)
+{
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.bInt(radapter::nativeValue<uint8_t>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.bInt());
+}
+
+napi_value radapter::Color::aInt(radapter::CallbackInfo& info)
+{
+    auto &color = info.unwrap<randar::Color>();
+
+    if (info.args.size()) {
+        color.aInt(radapter::nativeValue<uint8_t>(info.env, info.args[0]));
+        return info.self;
+    }
+
+    return napiValue(info.env, color.aInt());
 }
