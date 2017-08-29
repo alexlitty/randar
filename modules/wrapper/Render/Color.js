c7a5473965d07dee858c2eafbdaa4b7bf7e69df8
Alexander Litty <me@alexlitty.com>
2017-08-29 00:51:11 -0700

    Redefining color generation method.

--- modules/wrapper/Render/Color.js
+++ modules/wrapper/Render/Color.js
@@ -1,5 +1,16 @@
+const _ = require('underscore');
+
 module.exports = (randar) => {
-    randar.color = function() {
-        return new (randar.Color.bind.apply(randar.Color, arguments));
+    randar.color = function(r, g, b, a) {
+        if (arguments.length == 1 || arguments.length == 2) {
+            throw new Error('Invalid construction arguments');
+        }
+
+        return new randar.Color(
+            _.isUndefined(r) ? 0 : r,
+            _.isUndefined(b) ? 0 : g,
+            _.isUndefined(g) ? 0 : b,
+            _.isUndefined(a) ? 1 : a
+        );
     }
 };
