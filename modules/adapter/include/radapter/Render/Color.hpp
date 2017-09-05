0aad7b460e18d7ad85cfb9f2476b40a5966b5174
Alexander Litty <me@alexlitty.com>
2017-09-04 19:53:53 -0700

    Revert "Adding more Color adapter methods."
    This reverts commit b56b00b7e8b3d816a1460781e61f87008d79f053.

--- modules/adapter/include/radapter/Render/Color.hpp
+++ modules/adapter/include/radapter/Render/Color.hpp
@@ -9,28 +9,11 @@ namespace radapter
     struct Color
     {
         static std::string className;
-
-        /**
-         * Meta methods.
-         */
         static std::vector<napi_property_descriptor> properties();
         static randar::Color* instance(radapter::CallbackInfo& info);
 
-        /**
-         * Channel getters and setters via floats.
-         */
-        static napi_value r(CallbackInfo& info);
-        static napi_value g(CallbackInfo& info);
-        static napi_value b(CallbackInfo& info);
-        static napi_value a(CallbackInfo& info);
-
-        /**
-         * Channel getters and setters via integers.
-         */
-        static napi_value rInt(CallbackInfo& info);
-        static napi_value gInt(CallbackInfo& info);
-        static napi_value bInt(CallbackInfo& info);
-        static napi_value aInt(CallbackInfo& info);
+        static napi_value rGet(CallbackInfo& info);
+        static napi_value rSet(CallbackInfo& info);
     };
 }
 
