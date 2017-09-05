32fc795c9caa8230d2dd176616147e3b0d63c3c9
Alexander Litty <me@alexlitty.com>
2017-09-04 19:54:02 -0700

    Revert "Adding more napiValue specializations."
    This reverts commit 7af592bcdba2ce6018c7be9651185d5f824da186.

--- modules/adapter/include/radapter/Utility/NapiValue.hpp
+++ modules/adapter/include/radapter/Utility/NapiValue.hpp
@@ -5,58 +5,23 @@
 
 namespace radapter
 {
-    /**
-     * Base napiValue template.
-     *
-     * A specialization must be used.
-     */
-    template <typename T> struct compileTimeFalse : std::false_type { };
+    /*napi_value napiValue(napi_env env)
+    {
+        napi_value result;
+        checkNapi("get napi undefined object", env, napi_get_undefined(
+            env,
+            &result
+        ));
+        return result;
+    }*/
+
     template <typename T> napi_value napiValue(napi_env env, T value)
     {
-        // Asserts false if this base case is ever selected.
-        static_assert(compileTimeFalse<T>::value, "Conversion not implemented");
-
-        // Prevent the compiler from complaining about no return value.
-        return 0;
+        throw std::runtime_error("Conversion not implemented");
     }
 
-    /**
-     * Floating point specializations.
-     *
-     * Floats are converted to doubles.
-     */
-    template <> napi_value napiValue(napi_env env, float value);
     template <> napi_value napiValue(napi_env env, double value);
-
-    /**
-     * Integer specializations.
-     *
-     * JavaScript does not support typical integer types. They are represented
-     * as Numbers, which have enough precision to accurately represent 32 bit
-     * integers.
-     *
-     * A 64 bit integer "type" is exposed through N-API but only as a
-     * convenience. It does not offer accurate precision through the entire
-     * 64 bit range. For this reason, I suspect, an unsigned equivalent is not
-     * exposed.
-     */
-    template <> napi_value napiValue(napi_env env, int8_t value);
-    template <> napi_value napiValue(napi_env env, int16_t value);
-    template <> napi_value napiValue(napi_env env, int32_t value);
-    template <> napi_value napiValue(napi_env env, int64_t value);
-
-    template <> napi_value napiValue(napi_env env, uint8_t value);
-    template <> napi_value napiValue(napi_env env, uint16_t value);
-    template <> napi_value napiValue(napi_env env, uint32_t value);
-
-    /**
-     * String specialization.
-     *
-     * Strings are treated as UTF-8 here. While they are not natively treated as
-     * such, it suffices for Randar's needs.
-     */
-    template <> napi_value napiValue(napi_env env, const char* value);
-    template <> napi_value napiValue(napi_env env, std::string value);
+    template <> napi_value napiValue(napi_env env, float value);
 }
 
 #endif
