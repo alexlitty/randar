1c4c8b61530c775d6c10477348b024a80386f386
Alexander Litty <me@alexlitty.com>
2017-10-08 17:12:30 -0700

    Adding set tests to Transform.

--- test/tests/Math/Transform.js
+++ test/tests/Math/Transform.js
@@ -56,6 +56,18 @@ describe('Transform', function() {
             );
         });
 
+        it('accepts position and axis of rotation', function() {
+            assertTransform(
+                randar.transform(
+                    randar.vector(0.1, 0.2, 0.3),
+                    randar.vector(0.4, 0.5, 0.6)
+                ),
+                0.1, 0.2, 0.3,
+                0.46, 0.57, 0.68,
+                0
+            );
+        });
+
         it('accepts position, axis of rotation, and angle', function() {
             assertTransform(
                 randar.transform(
@@ -71,13 +83,97 @@ describe('Transform', function() {
 
         it('accepts quaternion', function() {
             assertTransform(
-                randar.transform(randar.quaternion(
-                    randar.vector(0.5, 0, 0),
-                    randar.angle(0.6)
-                )),
+                randar.transform(
+                    randar.quaternion(
+                        randar.vector(0.5, 0, 0),
+                        randar.angle(0.6)
+                    )
+                ),
                 0, 0, 0,
                 1, 0, 0,
                 0.6
+            );
+        });
+
+        it('accepts axis of rotation and angle', function() {
+            assertTransform(
+                randar.transform(
+                    randar.vector(67, 0, 0),
+                    randar.angle(0.42)
+                ),
+                0, 0, 0,
+                1, 0, 0,
+                0.42
+            );
+        });
+    });
+
+    describe('sets', function() {
+        it('resets', function() {
+            let transform = randar.transform(
+                randar.vector(1, 1, 1),
+                randar.quaternion(randar.vector(0, 0, 1), randar.angle(0.1))
+            );
+
+            assertTransform(
+                transform,
+                1, 1, 1,
+                0, 0, 1,
+                0.1
+            );
+
+            transform.reset();
+            assertTransform(
+                transform,
+                0, 0, 0,
+                0, 0, 0,
+                0
+            );
+        });
+
+        it('from position and quaternion', function() {
+            let transform = randar.transform();
+            transform.set(
+                randar.vector(2, 2, 2),
+                randar.quaternion(randar.vector(0, 0.3, 0), randar.angle(0.2))
+            );
+
+            assertTransform(
+                transform,
+                2, 2, 2,
+                0, 1, 0,
+                0.2
+            );
+        });
+
+        it('from position and axis of rotation', function() {
+            let transform = randar.transform();
+            transform.set(
+                randar.vector(3, 3, 3),
+                randar.vector(0.4, 0, 0)
+            );
+
+            assertTransform(
+                transform,
+                3, 3, 3,
+                1, 0, 0,
+                0
+            );
+        });
+
+        it('from position, axis of rotation, and angle', function() {
+            let transform = randar.transform();
+            transform.set(
+                randar.vector(4, 4, 4),
+                randar.vector(0, 0, 0.5),
+                randar.angle(0.67)
+            );
+
+            assertTransform(
+                transform,
+                4, 4, 4,
+                0, 0, 1,
+                0.67
             );
         });
     });
