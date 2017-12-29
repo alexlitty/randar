7d74287878d02d4f39b1ef3ceea9559edaf5012a
Alexander Litty <me@alexlitty.com>
2017-12-29 13:38:20 -0800

    Cleaning up old Texture ctx helpers.

--- test/tests/System/GraphicsContext.js
+++ test/tests/System/GraphicsContext.js
@@ -1,6 +1,5 @@
 describe('GraphicsContext', function() {
     it('constructs with OpenGL 4.5', function() {
-        let ctx = new randar.GraphicsContext();
-        assert(/^4.5 ?/.test(ctx.version()));
+        assert(/^4.5 ?/.test(randar.context().version()));
     });
 });
